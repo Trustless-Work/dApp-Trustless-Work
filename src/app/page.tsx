48cbfe30128de4bd7f995b3e147b8ccea949cbc9
JoelVR17 <vermudo.com@gmail.com>
2024-11-06 15:06:51 -0600

    feat: add welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function HomePage() {
+"use client";
+
+import Bounded from "@/components/Bounded";
+
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold text-gray-300">Trustless Work</h1>
-    </main>
+    <Bounded center={true}>
+      <div className="flex flex-col md:flex-row justify-center items-center w-full h-full mt-0 md:mt-20 gap-10">
+        <h1 className="text-5xl md:text-6xl font-bold">
+          Welcome to <br />
+          Trustless Work
+        </h1>
+        <hr className="hidden md:block bg-gray-200 w-0.5 h-96" />
+        <p className="text-xl">
+          <span className="text-primary font-bold">Escrow-as-a-service</span>{" "}
+          platform built on <strong>Soroban</strong>, <br />
+          <strong>Stellar's smart contract platform</strong>, designed to
+          provide secure,
+          <br />
+          transparent, and agile escrow solutions.
+        </p>
+      </div>
+    </Bounded>
   );
 }
