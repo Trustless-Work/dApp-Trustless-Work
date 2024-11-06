842804daac9389994d58a85136c7cb27e1d59224
JoelVR17 <vermudo.com@gmail.com>
2024-11-06 15:26:21 -0600

    feat: add initialize escrow service

---
+++ src/services/escrow/initializeEscrow.ts
@@ -0,0 +1,40 @@
+import { kit } from "@/wallet/walletKit";
+import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+interface EscrowPayload {
+  engagementId: string;
+  description: string;
+  serviceProvider: string;
+  amount: string;
+  signer: string;
+}
+
+export const initializeEscrow = async (payload: EscrowPayload) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/deployer/invoke-deployer-contract`,
+      payload
+    );
+    // const { address } = await kit.getAddress();
+    // const { signedTxXdr } = await kit.signTransaction(response.data, {
+    //   address,
+    //   networkPassphrase: WalletNetwork.TESTNET,
+    // });
+    // const tx = await axios.post(`${API_URL}/helper/send-transaction`, {
+    //   signedXdr: signedTxXdr,
+    // });
+    // const { data } = tx;
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Error:", error.message);
+      throw error;
+    } else {
+      console.error("Error:", error);
+      throw new Error("Error");
+    }
+  }
+};
