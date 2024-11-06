024d5baf8eab5bc56222c2127fd46ac82a814685
JoelVR17 <vermudo.com@gmail.com>
2024-11-06 15:26:08 -0600

    change: delete some imports

--- src/services/escrow/getEngagement.ts
+++ src/services/escrow/getEngagement.ts
@@ -1,7 +1,4 @@
 import axios from "axios";
-import { kit } from "@/wallet/walletKit";
-import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
-import { signTransaction } from "@stellar/freighter-api";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -14,7 +11,7 @@ export const getEngagement = async (payload: EscrowPayload) => {
   try {
     const { contractId, engagementId } = payload;
     const response = await axios.get(
-      `${API_URL}/escrow/get-escrow-by-engagement-id?contractId=${contractId}&engagementId=${engagementId}`,
+      `${API_URL}/escrow/get-escrow-by-engagement-id?contractId=${contractId}&engagementId=${engagementId}`
     );
     return response;
   } catch (error) {
