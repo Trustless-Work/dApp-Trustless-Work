2c803cb7bc95e8feebbbdd4e1fee9dc058e4b3da
Joel Vargas <vermudo.com@gmail.com>
2024-12-04 20:16:41 -0600

    feat: improve header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -50,42 +50,44 @@ const Header = () => {
   const pathName = usePathname();
 
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-      <div className="flex items-center gap-2 px-4">
-        <Separator orientation="vertical" className="mr-2 h-4" />
+    <header className="flex flex-1 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 mb-4">
+      <div className="flex w-full justify-between items-center gap-2 px-4">
         {pathName !== "/" && address ? (
           <>
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
+                  <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                  <ThemeToggle />
+                  <BreadcrumbPage>Example</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
 
-            <button
-              type="button"
-              onClick={handleDisconnect}
-              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center"
-            >
-              Disconnect
-            </button>
+            <div className="flex gap-5 ml-auto">
+              <ThemeToggle />
+              <button
+                type="button"
+                onClick={handleDisconnect}
+                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center"
+              >
+                Disconnect
+              </button>
+            </div>
           </>
         ) : (
-          <button
-            type="button"
-            onClick={handleConnect}
-            className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center"
-          >
-            Connect
-          </button>
+          <div className="flex gap-5 ml-auto">
+            <ThemeToggle />
+            <button
+              type="button"
+              onClick={handleConnect}
+              className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-3 text-center"
+            >
+              Connect
+            </button>
+          </div>
         )}
       </div>
     </header>
