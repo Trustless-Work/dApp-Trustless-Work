4d9e19892b0a1685f77970e8fc59158710587fc9
Joel Vargas <vermudo.com@gmail.com>
2024-12-06 17:24:26 -0600

    fix: remove automatic redirection on wallet disconnection

--- src/wallet/hooks/useWallet.hook.ts
+++ src/wallet/hooks/useWallet.hook.ts
@@ -1,7 +1,6 @@
 import { ISupportedWallet } from "@creit.tech/stellar-wallets-kit";
 import { useWalletStore } from "@/store/walletStore";
 import { kit } from "../walletKit";
-import { redirect } from "next/navigation";
 
 export const useWallet = () => {
   const { connectWalletStore, disconnectWalletStore } = useWalletStore();
@@ -23,7 +22,6 @@ export const useWallet = () => {
   const disconnectWallet = async () => {
     await kit.disconnect();
     disconnectWalletStore();
-    redirect("/");
   };
 
   return {
