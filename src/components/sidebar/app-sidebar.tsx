1da4ddc4704efded85229f7fa341798872df9fe6
Joel Vargas <vermudo.com@gmail.com>
2024-12-06 17:24:13 -0600

    feat: refactor sidebar component to use centralized sidebar items constant

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { BookOpen } from "lucide-react";
-import { FaStackOverflow } from "react-icons/fa";
 import { NavMain } from "@/components/sidebar/nav-main";
 import { NavUser } from "@/components/sidebar/nav-user";
 import { TeamSwitcher } from "@/components/sidebar/team-switcher";
-import { LuLayoutDashboard } from "react-icons/lu";
 import {
   Sidebar,
   SidebarContent,
@@ -14,76 +11,16 @@ import {
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar";
-
-const basePath = "/dashboard/escrow";
-
-const data = {
-  teams: [
-    {
-      name: "Trustless Work",
-      logo: "/logo.png",
-      plan: "Escrows as a service",
-    },
-  ],
-  navMain: [
-    {
-      title: "Dashboard",
-      url: "/dashboard",
-      icon: LuLayoutDashboard,
-      isActive: true,
-      isExpandable: false,
-    },
-    {
-      title: "Escrows",
-      url: "#",
-      icon: FaStackOverflow,
-      isActive: false,
-      isExpandable: true,
-      items: [
-        { title: "Initialize Escrow", url: `${basePath}/initialize-escrow` },
-        { title: "Fund Escrow", url: `${basePath}/fund-escrow` },
-        { title: "Complete Escrow", url: `${basePath}/complete-escrow` },
-        {
-          title: "Claim Escrow Earnings",
-          url: `${basePath}/claim-escrow-earnings`,
-        },
-        { title: "Cancel Escrow", url: `${basePath}/cancel-escrow` },
-        {
-          title: "Refund Remaining Funds",
-          url: `${basePath}/refund-remaining-funds`,
-        },
-        { title: "Get Engagement", url: `${basePath}/get-engagement` },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      isExpandable: true,
-      items: [
-        {
-          title: "API Documentation",
-          url: "https://docs.trustlesswork.com/trustless-work",
-          isExternal: true,
-        },
-        {
-          title: "Website",
-          url: "https://www.trustlesswork.com/",
-          isExternal: true,
-        },
-      ],
-    },
-  ],
-};
+import { ItemsSidebar } from "@/constants/Items.Sidebar";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <TeamSwitcher teams={ItemsSidebar.teams} />
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
+        <NavMain items={ItemsSidebar.navMain} />
       </SidebarContent>
       <SidebarFooter>
         <NavUser />
