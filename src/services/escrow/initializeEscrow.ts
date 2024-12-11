a20cc5b084ee42a1c6352c62e8736605a5d2ff21
Joel Vargas <vermudo.com@gmail.com>
2024-12-11 17:06:29 -0600

    feat: enhance initializeEscrow function with engagementId, platformFee, and disputeResolver fields

--- src/services/escrow/initializeEscrow.ts
+++ src/services/escrow/initializeEscrow.ts
@@ -1,26 +1,55 @@
 import http from "@/core/axios/http";
+import { kit } from "@/wallet/walletKit";
+import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
+import { signTransaction } from "@stellar/freighter-api";
 import axios from "axios";
 
 interface EscrowPayload {
+  engagementId: string;
   client: string;
   serviceProvider: string;
   platformAddress: string;
+  platformFee: string;
   amount: string;
   releaseSigner: string;
   milestones: {
     description: string;
     status: string;
+    flag?: boolean;
   }[];
-  signer: string;
+  disputeResolver: string;
 }
 
 export const initializeEscrow = async (payload: EscrowPayload) => {
   try {
+    const payloadWithFlag: EscrowPayload = {
+      ...payload,
+      milestones: payload.milestones.map((milestone) => ({
+        ...milestone,
+        flag: false,
+      })),
+    };
+
     const response = await http.post(
       "/deployer/invoke-deployer-contract",
-      payload,
+      payloadWithFlag,
     );
-    return response.data;
+
+    const { unsignedTransaction } = response.data;
+    const { address } = await kit.getAddress();
+
+    const { signedTxXdr } = await signTransaction(unsignedTransaction, {
+      address,
+      networkPassphrase: WalletNetwork.TESTNET,
+    });
+
+    const tx = await http.post("/helper/send-transaction", {
+      signedXdr: signedTxXdr,
+      returnValueIsRequired: true,
+    });
+
+    const { data } = tx;
+    return data;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       console.error("Axios Error:", error.response?.data || error.message);
