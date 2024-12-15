8d6264b609c655c9bb55a7ba940cfd4d3818c317
Renzo Espinosa <renzoespinosa.b@gmail.com>
2024-12-15 05:29:22 +0000

    feat: add contact form hook to add requests to the database and validations

--- src/components/modules/contact/hooks/contact-form.hook.ts
+++ src/components/modules/contact/hooks/contact-form.hook.ts
@@ -3,11 +3,17 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
 import { useToast } from "@/hooks/use-toast"
+import { collection, addDoc,query, where, getDocs } from "firebase/firestore/lite";
+import { firebaseDB } from "../../../../../firebase";
 
 const formSchema = z.object({
   firstName: z.string().min(1, { message: "First name is required" }),
   lastName: z.string().min(1, { message: "Last name is required" }),
-  email: z.string().email({ message: "Invalid email address" }),
+  email: z.string().email({ message: "Invalid email address" }).refine(
+    (email) =>
+      /^[^\s@]+@[^\s@]+\.[a-z]{2,}$/i.test(email), // Custom regex for additional validation
+    { message: "Invalid email domain." }
+  ),
   type: z.enum(["company", "individual"], { required_error: "Please select a type" }),
   description: z.string().min(10, { message: "Description must be at least 10 characters" }),
 })
@@ -33,9 +39,34 @@ export const useContactForm = () => {
     setIsLoading(true)
     try {
       // Mock API call for email uniqueness check and Firebase insertion
-      await new Promise(resolve => setTimeout(resolve, 1000))
+      const { firstName: name, lastName, email, type, description } = data
+
+      const q = query(
+        collection(firebaseDB, "api keys"),
+        where("email", "==", email)
+      );
+  
+      const querySnapshot = await getDocs(q);
+      if (!querySnapshot.empty) {
+        console.log("Email already exists!");
+        toast({
+          title: "Error",
+          description: "An API key has already been requested with this email.",
+          variant: "destructive",
+        })
+        return; // If it already exists, do not add the new record.
+      }
       
-      // Simulating a successful response
+      //  If it doesn't exist, add the new record
+      await addDoc(collection(firebaseDB, "api keys"), {
+        name,
+        lastName,
+        email,
+        type,
+        description,
+        createdAt: new Date().toISOString(),
+      });
+      console.log("Document successfully added!");
       toast({
         title: "Success",
         description: "Your escrow has been funded successfully.",
