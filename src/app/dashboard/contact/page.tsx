2a14ff68d7224ab644f059bc3af56d14e15f1f7e
jonvarsol <jonvarsol@gmail.com>
2024-12-18 12:59:04 -0600

    feat: create contact page

---
+++ src/app/dashboard/contact/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Bounded } from "@/components/Bounded";
+import { ContactForm } from "@/components/modules/contact/ContactForm";
+import { WrapperForm } from "@/components/Wrappers";
+
+export default function Contacts() {
+  return (
+    <Bounded center={true}>
+      <WrapperForm>
+        <h1 className="text-4xl font-bold">Contact</h1>
+        <h2>Fill in the details below to add a contact wallet.</h2>
+        <ContactForm />
+      </WrapperForm>
+    </Bounded>
+  );
+}
