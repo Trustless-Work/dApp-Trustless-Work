9526b1fcd7d05c4af5e4a2de97eb03bc37f4035b
Collins Ikechukwu <collinschristroa@gmail.com>
2024-12-22 14:58:51 +0100

    feat(escrowFormStore): add Zustand store for escrow form state management

---
+++ src/store/escrowFormStore/store.ts
@@ -0,0 +1,48 @@
+import { create } from 'zustand';
+
+interface Milestone {
+  description: string;
+  status: string;
+}
+
+interface EscrowFormData {
+  client: string;
+  engagementId: string;
+  serviceProvider: string;
+  platformAddress: string;
+  platformFee: string;
+  amount: string;
+  releaseSigner: string;
+  disputeResolver: string;
+  milestones: Milestone[];
+}
+
+interface EscrowFormState {
+  formData: EscrowFormData;
+  setFormData: (data: Partial<EscrowFormData>) => void;
+  resetForm: () => void;
+}
+
+const initialState: EscrowFormData = {
+  client: '',
+  engagementId: '',
+  serviceProvider: '',
+  platformAddress: '',
+  platformFee: '',
+  amount: '',
+  releaseSigner: '',
+  disputeResolver: '',
+  milestones: [{ description: '', status: '' }],
+};
+
+export const useEscrowFormStore = create<EscrowFormState>((set) => ({
+  formData: initialState,
+  setFormData: (data) =>
+    set((state) => ({
+      formData: {
+        ...state.formData,
+        ...data,
+      },
+    })),
+  resetForm: () => set({ formData: initialState }),
+}));
