4ddac90af4875ea80fbb38c7c84951c3e079f471
JoelVR17 <vermudo.com@gmail.com>
2024-12-25 10:17:16 -0600

    refactor(settings): update import paths for Profile, Appearance, and Preferences sections

--- src/components/modules/setting/Settings.tsx
+++ src/components/modules/setting/Settings.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/layout/header/Header";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import SettingsSidebar from "./Sidebar";
-import ProfileSection from "./ProfileSection";
-import AppearanceSection from "./AppearanceSection";
-import PreferencesSection from "./PreferencesSection";
 import useSettings from "./hooks/settings.hook";
+import ProfileSection from "./profileSection";
+import AppearanceSection from "./appearanceSection";
+import PreferencesSection from "./preferencesSection";
 
 const Settings = () => {
   const { currentTab, setCurrentTab, saveProfile, theme, toggleTheme } =
