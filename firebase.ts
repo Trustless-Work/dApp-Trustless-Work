03d3e503fc61f5e42c670595db78ba146e321935
JoelVR17 <vermudo.com@gmail.com>
2025-01-03 14:44:36 -0600

    refactor: remove unused Firebase analytics import and initialization

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 import { getFirestore } from "firebase/firestore/lite";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,6 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(firebaseApp);
 
 export const firebaseDB = getFirestore(firebaseApp);
