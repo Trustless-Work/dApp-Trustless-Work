ddc2a54f418e6120e6679b43eac2ac4867341705
JoelVR17 <vermudo.com@gmail.com>
2025-01-04 13:37:52 -0600

    refactor: rename my-escrows.ts to my-escrows.hook.ts and update state management to use global store

--- src/components/modules/escrow/hooks/my-escrows.hook.ts
+++ src/components/modules/escrow/hooks/my-escrows.hook.ts
@@ -1,30 +1,32 @@
 import { useWalletStore } from "@/store/walletStore/store";
 import { useState, useEffect, useMemo } from "react";
-import { getAllEscrowsByUser } from "../server/escrow-firebase";
-import { Escrow } from "@/@types/escrow.entity";
+import { useGlobalBoundedStore } from "@/core/store";
 
 const useMyEscrows = () => {
   const { address } = useWalletStore();
+  const fetchAllEscrows = useGlobalBoundedStore(
+    (state) => state.fetchAllEscrows,
+  );
+  const escrows = useGlobalBoundedStore((state) => state.escrows);
+  const totalEscrows = useGlobalBoundedStore((state) => state.totalEscrows);
 
   const [expandedRows, setExpandedRows] = useState<string[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(15);
-  const [fetchedEscrows, setFetchedEscrows] = useState<Escrow[]>([]);
 
-  const totalPages = Math.ceil(fetchedEscrows.length / itemsPerPage);
+  const totalPages = Math.ceil(totalEscrows / itemsPerPage);
 
   const currentData = useMemo(() => {
-    return fetchedEscrows.slice(
+    return escrows.slice(
       (currentPage - 1) * itemsPerPage,
       currentPage * itemsPerPage,
     );
-  }, [fetchedEscrows, currentPage, itemsPerPage]);
+  }, [escrows, currentPage, itemsPerPage]);
 
   useEffect(() => {
     const fetchEscrows = async () => {
       if (address) {
-        const res = await getAllEscrowsByUser({ address });
-        setFetchedEscrows(res.data);
+        fetchAllEscrows({ address, type: "client" });
       }
     };
 
