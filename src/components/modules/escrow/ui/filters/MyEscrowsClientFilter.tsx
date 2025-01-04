030be1612d7d4a6284860510c8b6d4de0d5c7fd6
JoelVR17 <vermudo.com@gmail.com>
2025-01-04 14:34:37 -0600

    feat: implement MyEscrowsClientFilter component with search and select options

--- src/components/modules/escrow/ui/filters/MyEscrowsClientFilter.tsx
+++ src/components/modules/escrow/ui/filters/MyEscrowsClientFilter.tsx
@@ -1,8 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import Divider from "@/components/utils/Divider";
+import { Search, Trash2 } from "lucide-react";
+
 const MyEscrowsClientFilter = () => {
   return (
-    <>
-      <h1>filters client</h1>
-    </>
+    <form className="flex flex-col space-y-5">
+      <div className="flex flex-col w-1/4">
+        <div className="flex items-center space-x-2">
+          <Input id="search" placeholder="Search..." />
+          <Search className="h-5 w-5" />
+        </div>
+      </div>
+
+      <Divider type="horizontal" />
+
+      <div className="flex gap-3">
+        <div className="flex flex-col">
+          <label className="text-xs font-bold mb-2 ml-2" htmlFor="select1">
+            Filtro 1
+          </label>
+          <Select>
+            <SelectTrigger>
+              <span>Selecciona una opción</span>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="option1">Opción 1</SelectItem>
+              <SelectItem value="option2">Opción 2</SelectItem>
+              <SelectItem value="option3">Opción 3</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div className="flex flex-col">
+          <label className="text-xs font-bold mb-2 ml-2" htmlFor="select1">
+            Filtro 2
+          </label>
+          <Select>
+            <SelectTrigger>
+              <span>Selecciona otra opción</span>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="optionA">Opción A</SelectItem>
+              <SelectItem value="optionB">Opción B</SelectItem>
+              <SelectItem value="optionC">Opción C</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    </form>
   );
 };
 
