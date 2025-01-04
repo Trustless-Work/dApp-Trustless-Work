7e1a6aa15e8d20e0edd806b7819f2433db39a154
JoelVR17 <vermudo.com@gmail.com>
2025-01-04 13:37:42 -0600

    feat: enhance MyEscrowsDisputeResolverTable with NoData component and improve conditional rendering

--- src/components/modules/escrow/ui/tables/MyEscrowsClientTable.tsx
+++ src/components/modules/escrow/ui/tables/MyEscrowsClientTable.tsx
@@ -10,8 +10,9 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useFormatUtils } from "@/utils/hook/format.hook";
-import useMyEscrows from "../../hooks/my-escrows";
+import useMyEscrows from "../../hooks/my-escrows.hook";
 import React from "react";
+import NoData from "@/components/utils/NoData";
 
 const MyEscrowsClientTable = () => {
   const {
@@ -30,109 +31,120 @@ const MyEscrowsClientTable = () => {
 
   return (
     <div className="container mx-auto py-3">
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Title</TableHead>
-              <TableHead>Description</TableHead>
-              <TableHead>Amount</TableHead>
-              <TableHead>Engagement</TableHead>
-              <TableHead>Platform</TableHead>
-              <TableHead>Platform Fee</TableHead>
-              <TableHead>Client</TableHead>
-              <TableHead>Dispute Resolver</TableHead>
-              <TableHead>Release Signer</TableHead>
-              <TableHead>Service Provider</TableHead>
-              <TableHead>Created</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {currentData.map((row: Escrow) => (
-              <React.Fragment key={row.id}>
-                <TableRow
-                  key={row.id}
-                  className="animate-fade-in"
-                  onClick={() => toggleRowExpansion(row.id)}
-                >
-                  <TableCell className="font-medium">{row.title}</TableCell>
-                  <TableCell>{row.description}</TableCell>
-                  <TableCell>{row.amount}</TableCell>
-                  <TableCell>{row.engagementId}</TableCell>
-                  <TableCell>{formatAddress(row.platformAddress)}</TableCell>
-                  <TableCell>{row.platformFee}</TableCell>
-                  <TableCell>{formatAddress(row.client)}</TableCell>
-                  <TableCell>
-                    {formatDateFromFirebase(
-                      row.createdAt.seconds,
-                      row.createdAt.nanoseconds,
-                    )}
-                  </TableCell>
-                  <TableCell>{formatAddress(row.disputeResolver)}</TableCell>
-                  <TableCell>{formatAddress(row.releaseSigner)}</TableCell>
-                  <TableCell>{formatAddress(row.serviceProvider)}</TableCell>
-                  <TableCell>
-                    <p
-                      className="w-5 h-5 cursor-pointer border border-gray-400 dark:border-gray-500 rounded-lg flex justify-center items-center"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        toggleRowExpansion(row.id);
-                      }}
-                    >
-                      {expandedRows.includes(row.id) ? "-" : "+"}
-                    </p>
-                  </TableCell>
+      {currentData.length !== 0 ? (
+        <>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Title</TableHead>
+                  <TableHead>Description</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead>Engagement</TableHead>
+                  <TableHead>Platform</TableHead>
+                  <TableHead>Platform Fee</TableHead>
+                  <TableHead>Client</TableHead>
+                  <TableHead>Dispute Resolver</TableHead>
+                  <TableHead>Release Signer</TableHead>
+                  <TableHead>Service Provider</TableHead>
+                  <TableHead>Created</TableHead>
                 </TableRow>
-
-                {row.milestones && expandedRows.includes(row.id) && (
-                  <TableRow>
-                    <TableCell colSpan={8} className="p-4">
-                      <div>
-                        <p>
-                          <strong>Milestones:</strong>
+              </TableHeader>
+              <TableBody>
+                {currentData.map((row: Escrow) => (
+                  <React.Fragment key={row.id}>
+                    <TableRow
+                      className="animate-fade-in"
+                      onClick={() => toggleRowExpansion(row.id)}
+                    >
+                      <TableCell className="font-medium">{row.title}</TableCell>
+                      <TableCell>{row.description}</TableCell>
+                      <TableCell>{row.amount}</TableCell>
+                      <TableCell>{row.engagementId}</TableCell>
+                      <TableCell>
+                        {formatAddress(row.platformAddress)}
+                      </TableCell>
+                      <TableCell>{row.platformFee}</TableCell>
+                      <TableCell>{formatAddress(row.client)}</TableCell>
+                      <TableCell>
+                        {formatDateFromFirebase(
+                          row.createdAt.seconds,
+                          row.createdAt.nanoseconds,
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        {formatAddress(row.disputeResolver)}
+                      </TableCell>
+                      <TableCell>{formatAddress(row.releaseSigner)}</TableCell>
+                      <TableCell>
+                        {formatAddress(row.serviceProvider)}
+                      </TableCell>
+                      <TableCell>
+                        <p
+                          className="w-5 h-5 cursor-pointer border border-gray-400 dark:border-gray-500 rounded-lg flex justify-center items-center"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            toggleRowExpansion(row.id);
+                          }}
+                        >
+                          {expandedRows.includes(row.id) ? "-" : "+"}
                         </p>
-                        <ul>
-                          {row.milestones.map((milestone, index) => (
-                            <li key={index}>{milestone.description}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </React.Fragment>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+                      </TableCell>
+                    </TableRow>
 
-      <div className="flex justify-center items-center mt-4 space-x-2">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <Button
-            key={index}
-            onClick={() => handlePageChange(index + 1)}
-            className={
-              currentPage === index + 1
-                ? "bg-primary text-white"
-                : "bg-gray-200 text-gray-700 hover:text-white"
-            }
-          >
-            {index + 1}
-          </Button>
-        ))}
-        <Input
-          id="itemsPerPage"
-          type="number"
-          min="1"
-          max={currentData.length}
-          value={itemsPerPage}
-          onChange={(e) => {
-            setItemsPerPage(Number(e.target.value) || 1);
-            setCurrentPage(1);
-          }}
-          className="w-20"
-        />
-      </div>
+                    {row.milestones && expandedRows.includes(row.id) && (
+                      <TableRow>
+                        <TableCell colSpan={8} className="p-4">
+                          <div>
+                            <p>
+                              <strong>Milestones:</strong>
+                            </p>
+                            <ul>
+                              {row.milestones.map((milestone, index) => (
+                                <li key={index}>{milestone.description}</li>
+                              ))}
+                            </ul>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </React.Fragment>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+
+          <div className="flex justify-center items-center mt-4 space-x-2">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <Button
+                key={index}
+                onClick={() => handlePageChange(index + 1)}
+                className={
+                  currentPage === index + 1
+                    ? "bg-primary text-white"
+                    : "bg-gray-200 text-gray-700 hover:text-white"
+                }
+              >
+                {index + 1}
+              </Button>
+            ))}
+            <Input
+              id="itemsPerPage"
+              type="number"
+              min="1"
+              max={currentData.length}
+              value={itemsPerPage}
+              onChange={(e) => {
+                setItemsPerPage(Number(e.target.value) || 1);
+                setCurrentPage(1);
+              }}
+              className="w-20"
+            />
+          </div>
+        </>
+      ) : (
+        <NoData />
+      )}
     </div>
   );
 };
--- src/components/modules/escrow/ui/tables/MyEscrowsDisputeResolverTable.tsx
+++ src/components/modules/escrow/ui/tables/MyEscrowsDisputeResolverTable.tsx
@@ -10,7 +10,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useFormatUtils } from "@/utils/hook/format.hook";
-import useMyEscrows from "../../hooks/my-escrows";
+import useMyEscrows from "../../hooks/my-escrows.hook";
+import NoData from "@/components/utils/NoData";
 
 const MyEscrowsDisputeResolverTable = () => {
   const {
@@ -29,109 +30,121 @@ const MyEscrowsDisputeResolverTable = () => {
 
   return (
     <div className="container mx-auto py-3">
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Title</TableHead>
-              <TableHead>Description</TableHead>
-              <TableHead>Amount</TableHead>
-              <TableHead>Engagement</TableHead>
-              <TableHead>Platform</TableHead>
-              <TableHead>Platform Fee</TableHead>
-              <TableHead>Client</TableHead>
-              <TableHead>Dispute Resolver</TableHead>
-              <TableHead>Release Signer</TableHead>
-              <TableHead>Service Provider</TableHead>
-              <TableHead>Created</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {currentData.map((row: Escrow) => (
-              <>
-                <TableRow
-                  key={row.id}
-                  className="animate-fade-in"
-                  onClick={() => toggleRowExpansion(row.id)}
-                >
-                  <TableCell className="font-medium">{row.title}</TableCell>
-                  <TableCell>{row.description}</TableCell>
-                  <TableCell>{row.amount}</TableCell>
-                  <TableCell>{row.engagementId}</TableCell>
-                  <TableCell>{formatAddress(row.platformAddress)}</TableCell>
-                  <TableCell>{row.platformFee}</TableCell>
-                  <TableCell>{formatAddress(row.client)}</TableCell>
-                  <TableCell>
-                    {formatDateFromFirebase(
-                      row.createdAt.seconds,
-                      row.createdAt.nanoseconds,
-                    )}
-                  </TableCell>
-                  <TableCell>{formatAddress(row.disputeResolver)}</TableCell>
-                  <TableCell>{formatAddress(row.releaseSigner)}</TableCell>
-                  <TableCell>{formatAddress(row.serviceProvider)}</TableCell>
-                  <TableCell>
-                    <p
-                      className="w-5 h-5 cursor-pointer border border-gray-400 dark:border-gray-500 rounded-lg flex justify-center items-center"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        toggleRowExpansion(row.id);
-                      }}
-                    >
-                      {expandedRows.includes(row.id) ? "-" : "+"}
-                    </p>
-                  </TableCell>
+      {currentData.length !== 0 ? (
+        <>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Title</TableHead>
+                  <TableHead>Description</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead>Engagement</TableHead>
+                  <TableHead>Platform</TableHead>
+                  <TableHead>Platform Fee</TableHead>
+                  <TableHead>Client</TableHead>
+                  <TableHead>Dispute Resolver</TableHead>
+                  <TableHead>Release Signer</TableHead>
+                  <TableHead>Service Provider</TableHead>
+                  <TableHead>Created</TableHead>
                 </TableRow>
-
-                {row.milestones && expandedRows.includes(row.id) && (
-                  <TableRow>
-                    <TableCell colSpan={8} className="p-4">
-                      <div>
-                        <p>
-                          <strong>Milestones:</strong>
+              </TableHeader>
+              <TableBody>
+                {currentData.map((row: Escrow) => (
+                  <>
+                    <TableRow
+                      key={row.id}
+                      className="animate-fade-in"
+                      onClick={() => toggleRowExpansion(row.id)}
+                    >
+                      <TableCell className="font-medium">{row.title}</TableCell>
+                      <TableCell>{row.description}</TableCell>
+                      <TableCell>{row.amount}</TableCell>
+                      <TableCell>{row.engagementId}</TableCell>
+                      <TableCell>
+                        {formatAddress(row.platformAddress)}
+                      </TableCell>
+                      <TableCell>{row.platformFee}</TableCell>
+                      <TableCell>{formatAddress(row.client)}</TableCell>
+                      <TableCell>
+                        {formatDateFromFirebase(
+                          row.createdAt.seconds,
+                          row.createdAt.nanoseconds,
+                        )}
+                      </TableCell>
+                      <TableCell>
+                        {formatAddress(row.disputeResolver)}
+                      </TableCell>
+                      <TableCell>{formatAddress(row.releaseSigner)}</TableCell>
+                      <TableCell>
+                        {formatAddress(row.serviceProvider)}
+                      </TableCell>
+                      <TableCell>
+                        <p
+                          className="w-5 h-5 cursor-pointer border border-gray-400 dark:border-gray-500 rounded-lg flex justify-center items-center"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            toggleRowExpansion(row.id);
+                          }}
+                        >
+                          {expandedRows.includes(row.id) ? "-" : "+"}
                         </p>
-                        <ul>
-                          {row.milestones.map((milestone, index) => (
-                            <li key={index}>{milestone.description}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+                      </TableCell>
+                    </TableRow>
 
-      <div className="flex justify-center items-center mt-4 space-x-2">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <Button
-            key={index}
-            onClick={() => handlePageChange(index + 1)}
-            className={
-              currentPage === index + 1
-                ? "bg-primary text-white"
-                : "bg-gray-200 text-gray-700 hover:text-white"
-            }
-          >
-            {index + 1}
-          </Button>
-        ))}
-        <Input
-          id="itemsPerPage"
-          type="number"
-          min="1"
-          max={currentData.length}
-          value={itemsPerPage}
-          onChange={(e) => {
-            setItemsPerPage(Number(e.target.value) || 1);
-            setCurrentPage(1);
-          }}
-          className="w-20"
-        />
-      </div>
+                    {row.milestones && expandedRows.includes(row.id) && (
+                      <TableRow>
+                        <TableCell colSpan={8} className="p-4">
+                          <div>
+                            <p>
+                              <strong>Milestones:</strong>
+                            </p>
+                            <ul>
+                              {row.milestones.map((milestone, index) => (
+                                <li key={index}>{milestone.description}</li>
+                              ))}
+                            </ul>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+
+          <div className="flex justify-center items-center mt-4 space-x-2">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <Button
+                key={index}
+                onClick={() => handlePageChange(index + 1)}
+                className={
+                  currentPage === index + 1
+                    ? "bg-primary text-white"
+                    : "bg-gray-200 text-gray-700 hover:text-white"
+                }
+              >
+                {index + 1}
+              </Button>
+            ))}
+            <Input
+              id="itemsPerPage"
+              type="number"
+              min="1"
+              max={currentData.length}
+              value={itemsPerPage}
+              onChange={(e) => {
+                setItemsPerPage(Number(e.target.value) || 1);
+                setCurrentPage(1);
+              }}
+              className="w-20"
+            />
+          </div>
+        </>
+      ) : (
+        <NoData />
+      )}
     </div>
   );
 };
