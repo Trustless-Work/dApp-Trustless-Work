c5a4d73ba2e5008c191193a176c30265b3bb9148
JoelVR17 <vermudo.com@gmail.com>
2025-01-04 13:58:08 -0600

    feat: add NoData component and update MyEscrowsCards to handle empty data state

--- src/components/utils/NoData.tsx
+++ src/components/utils/NoData.tsx
@@ -1,8 +1,14 @@
 import { AiOutlineFileSearch } from "react-icons/ai";
 
-const NoData = () => {
+interface NoDataProps {
+  isCard?: boolean;
+}
+
+const NoData = ({ isCard }: NoDataProps) => {
   return (
-    <div className="flex flex-col justify-center items-center my-5 gap-3 text-gray-500">
+    <div
+      className={`flex flex-col justify-center items-center my-5 gap-3 text-gray-500 ${isCard && "py-3"}`}
+    >
       <AiOutlineFileSearch size={50} />
       <h1 className="text-lg">No Data Available</h1>
     </div>
