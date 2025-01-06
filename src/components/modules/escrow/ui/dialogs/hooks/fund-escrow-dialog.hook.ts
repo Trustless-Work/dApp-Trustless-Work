272d1d612dcdddc569a3ce4b06fcc32dc024ecec
Joel Vargas <vermudo.com@gmail.com>
2025-01-06 16:47:03 -0600

    feat: add useFundEscrowDialogHook for managing escrow funding process with form validation and toast notifications

---
+++ src/components/modules/escrow/ui/dialogs/hooks/fund-escrow-dialog.hook.ts
@@ -0,0 +1,85 @@
+"sue client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { formSchema } from "../../../schema/fund-escrow-schema";
+import { useToast } from "@/hooks/use-toast";
+import { useLoaderStore } from "@/store/utilsStore/store";
+import { useWalletStore } from "@/store/walletStore/store";
+import { fundEscrow } from "@/services/escrow/fundEscrow";
+
+interface useFundEscrowDialogProps {
+  setIsSecondDialogOpen: (value: boolean) => void;
+}
+
+const useFundEscrowDialogHook = ({
+  setIsSecondDialogOpen,
+}: useFundEscrowDialogProps) => {
+  const { toast } = useToast();
+  const { address } = useWalletStore();
+  const setIsLoading = useLoaderStore((state) => state.setIsLoading);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      contractId: "",
+      amount: "",
+      engagementId: "",
+    },
+  });
+
+  const onSubmit = async (payload: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+
+    try {
+      const data = await fundEscrow({
+        contractId: payload.contractId,
+        signer: address,
+        amount: payload.amount,
+        engagementId: payload.engagementId,
+      });
+      if (data.status === "SUCCESS" || data.status === 201) {
+        // ! Validate if the user has the preference in true
+        //await addEscrow(payload, address, data.contract_id);
+        // ! UPDATE ESCROW IN FIREBASE
+
+        form.reset();
+        setIsSecondDialogOpen(false);
+        setIsLoading(false);
+
+        toast({
+          title: "Success",
+          description: data.message,
+        });
+      } else {
+        setIsLoading(false);
+        toast({
+          title: "Error",
+          description: data.message || "An error occurred",
+          variant: "destructive",
+        });
+      }
+    } catch (error: any) {
+      setIsLoading(false);
+      const errorMessage =
+        error.response && error.response.data
+          ? error.response.data.message
+          : "An error occurred";
+
+      toast({
+        title: "Error",
+        description: errorMessage,
+        variant: "destructive",
+      });
+    }
+  };
+
+  const handleClose = () => {
+    setIsSecondDialogOpen(false);
+  };
+
+  return { onSubmit, form, handleClose };
+};
+
+export default useFundEscrowDialogHook;
