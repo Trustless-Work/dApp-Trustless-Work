8f13faadf36fc8839d37333051cd221caa67d59e
Joel Vargas <vermudo.com@gmail.com>
2025-01-06 15:50:05 -0600

    feat: add EntityCard component for displaying entity information with optional progress percentage

---
+++ src/components/modules/escrow/ui/dialogs/components/EntityCard.tsx
@@ -0,0 +1,45 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { SidebarMenuButton } from "@/components/ui/sidebar";
+import { useFormatUtils } from "@/utils/hook/format.hook";
+
+interface EntityCardProps {
+  entity: string;
+  type: string;
+  hasPercentage?: boolean;
+  percentage?: string;
+}
+
+const EntityCard = ({
+  entity,
+  type,
+  hasPercentage,
+  percentage,
+}: EntityCardProps) => {
+  const { formatAddress } = useFormatUtils();
+
+  return (
+    <div className="flex flex-col w-1/3">
+      <h3 className="font-bold text-xs ml-2 mb-2 flex justify-between">
+        {type}{" "}
+        {hasPercentage && (
+          <span className="text-green-700 font-black">{percentage}%</span>
+        )}
+      </h3>
+      <SidebarMenuButton
+        size="lg"
+        className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+      >
+        <Avatar className="h-8 w-8 rounded-lg">
+          <AvatarImage src={""} alt={"name"} />
+          <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+        </Avatar>
+        <div className="grid flex-1 text-left text-sm leading-tight">
+          <span className="truncate text-xs font-semibold">{"name"}</span>
+          <span className="truncate text-xs">{formatAddress(entity)}</span>
+        </div>
+      </SidebarMenuButton>
+    </div>
+  );
+};
+
+export default EntityCard;
