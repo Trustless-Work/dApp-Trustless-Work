f9ac920c8af0a9982989e5a286ae61ecc52a3905
Joel Vargas <vermudo.com@gmail.com>
2025-01-06 16:47:14 -0600

    feat: add FundEscrowDialog component for funding existing escrow contracts with form inputs

---
+++ src/components/modules/escrow/ui/dialogs/FundEscrowDialog.tsx
@@ -0,0 +1,139 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import useFundEscrowDialog from "./hooks/fund-escrow-dialog.hook";
+import { useGlobalBoundedStore } from "@/core/store";
+import { Form } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import TooltipInfo from "@/components/utils/Tooltip";
+import useFundEscrowDialogHook from "./hooks/fund-escrow-dialog.hook";
+
+interface FundEscrowDialogProps {
+  isSecondDialogOpen: boolean;
+  setIsSecondDialogOpen: (value: boolean) => void;
+}
+
+const FundEscrowDialog = ({
+  isSecondDialogOpen,
+  setIsSecondDialogOpen,
+}: FundEscrowDialogProps) => {
+  const { form, onSubmit } = useFundEscrowDialogHook({
+    setIsSecondDialogOpen,
+  });
+
+  const { handleClose } = useFundEscrowDialog({
+    setIsSecondDialogOpen,
+  });
+
+  const selectedEscrow = useGlobalBoundedStore((state) => state.selectedEscrow);
+
+  return (
+    <Dialog open={isSecondDialogOpen} onOpenChange={handleClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Funding - {selectedEscrow?.title}</DialogTitle>
+          <DialogDescription>
+            Allows users to deposit funds into an existing escrow contract,
+            securing them until the agreed conditions are met.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="grid gap-4 py-4"
+          >
+            <div className="grid grid-cols-4 items-center gap-4">
+              <FormField
+                control={form.control}
+                name="contractId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center">
+                      ContractID
+                      <TooltipInfo content="The unique identifier of the contract." />
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Unique contract identifier"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="engagementId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center">
+                      Engagement
+                      <TooltipInfo content="The unique identifier linking this escrow to a specific project or transaction." />
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Engagement identifier"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center">
+                      Amount
+                      <TooltipInfo content="The amount to be funded." />
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="The amount to be funded"
+                        {...field}
+                        onChange={(e) => {
+                          field.onChange(e);
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <DialogFooter>
+              <Button type="submit">Fund</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default FundEscrowDialog;
