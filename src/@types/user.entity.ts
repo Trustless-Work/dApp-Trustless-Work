a3ce36361d56fee1a9151c732c04ca4d028c2222
Joel Vargas <vermudo.com@gmail.com>
2025-01-07 14:26:50 -0600

    feat: rename name property to firstName and add lastName to User interface

--- src/@types/user.entity.ts
+++ src/@types/user.entity.ts
@@ -2,7 +2,8 @@ export interface User {
   id: string;
   createdAt: string;
   updatedAt: string;
-  name?: string;
+  firstName?: string;
+  lastName?: string;
   email?: string;
   identification?: string;
 }
