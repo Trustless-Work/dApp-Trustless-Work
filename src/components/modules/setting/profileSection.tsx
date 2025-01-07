60914ef68d1627ce9dde189c1a4b62af48d0e1c3
Joel Vargas <vermudo.com@gmail.com>
2025-01-07 14:12:55 -0600

    feat: wrap ProfileSection in Card component for improved layout and styling

--- src/components/modules/setting/profileSection.tsx
+++ src/components/modules/setting/profileSection.tsx
@@ -12,6 +12,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import useProfile from "./hooks/profile-section.hook";
 import { UserPayload } from "@/@types/user.entity";
+import { Card, CardContent } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 interface ProfileSectionProps {
   onSave: (data: UserPayload) => void;
@@ -23,53 +25,57 @@ const ProfileSection = ({ onSave }: ProfileSectionProps) => {
   });
 
   return (
-    <section>
-      <h1 className="text-3xl font-bold mb-4">Profile</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="identification"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Identification</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your ID" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="firstName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>First Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your first name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="lastName"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Last Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your last name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Save Profile</Button>
-        </form>
-      </Form>
-    </section>
+    <Card className={cn("overflow-hidden")}>
+      <CardContent className="p-6">
+        <h1 className="text-3xl font-bold mb-4">Profile</h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="identification"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Identification</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your ID" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your first name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your last name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-end w-full">
+              <Button type="submit">Save Profile</Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 
