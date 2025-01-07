a1fa2a962c4eb54c2c1482ce0b840a42430445a6
Joel Vargas <vermudo.com@gmail.com>
2025-01-07 13:38:55 -0600

    feat: add loggedUser property to AuthenticationGlobalStore for user tracking

--- src/core/store/data/@types/authentication.entity.ts
+++ src/core/store/data/@types/authentication.entity.ts
@@ -1,6 +1,9 @@
+import { User } from "@/@types/user.entity";
+
 export interface AuthenticationGlobalStore {
   address: string;
   name: string;
+  loggedUser: User | null;
 
   connectWalletStore: (address: string, name: string) => void;
   disconnectWalletStore: () => void;
