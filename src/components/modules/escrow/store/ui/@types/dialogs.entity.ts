bee467f159a8d040d51285088f7d2abf1a64c82b
Joel Vargas <vermudo.com@gmail.com>
2025-01-15 14:24:02 -0600

    feat: add QR dialog state management to DialogEscrowStore interface

--- src/components/modules/escrow/store/ui/@types/dialogs.entity.ts
+++ src/components/modules/escrow/store/ui/@types/dialogs.entity.ts
@@ -1,6 +1,8 @@
 export interface DialogEscrowStore {
   isDialogOpen: boolean;
   isSecondDialogOpen: boolean;
+  isQRDialogOpen: boolean;
   setIsDialogOpen: (value: boolean) => void;
   setIsSecondDialogOpen: (value: boolean) => void;
+  setIsQRDialogOpen: (value: boolean) => void;
 }
