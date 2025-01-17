5a3384430c30062aec0ec7be3b7fcdac764c9f0d
Joel Vargas <vermudo.com@gmail.com>
2025-01-16 20:53:09 -0600

    feat: add state management for Resolve Dispute dialog in DialogEscrowStore

--- src/components/modules/escrow/store/ui/@types/dialogs.entity.ts
+++ src/components/modules/escrow/store/ui/@types/dialogs.entity.ts
@@ -2,7 +2,9 @@ export interface DialogEscrowStore {
   isDialogOpen: boolean;
   isSecondDialogOpen: boolean;
   isQRDialogOpen: boolean;
+  isResolveDisputeDialogOpen: boolean;
   setIsDialogOpen: (value: boolean) => void;
   setIsSecondDialogOpen: (value: boolean) => void;
   setIsQRDialogOpen: (value: boolean) => void;
+  setIsResolveDisputeDialogOpen: (value: boolean) => void;
 }
