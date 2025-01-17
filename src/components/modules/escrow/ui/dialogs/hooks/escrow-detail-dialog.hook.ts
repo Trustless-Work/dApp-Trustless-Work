1be554bd56b777d943bd2dc33b2355212098ff94
Joel Vargas <vermudo.com@gmail.com>
2025-01-16 18:49:08 -0600

    refactor: remove redundant milestone status and flag handling from escrow detail dialog hook

--- src/components/modules/escrow/ui/dialogs/hooks/escrow-detail-dialog.hook.ts
+++ src/components/modules/escrow/ui/dialogs/hooks/escrow-detail-dialog.hook.ts
@@ -1,9 +1,4 @@
-import {
-  Escrow,
-  EscrowPayload,
-  Milestone,
-  MilestoneStatus,
-} from "@/@types/escrow.entity";
+import { Escrow, EscrowPayload, Milestone } from "@/@types/escrow.entity";
 import { statusMap, statusOptions } from "@/constants/escrow/StatusOptions";
 import { getUserRoleInEscrow } from "../../../server/escrow-firebase";
 import {
@@ -11,7 +6,6 @@ import {
   useGlobalBoundedStore,
 } from "@/core/store/data";
 import { useEffect, useState } from "react";
-import { changeMilestoneStatus } from "../../../services/changeMilestoneStatus";
 import { useEscrowBoundedStore } from "../../../store/ui";
 import { useToast } from "@/hooks/use-toast";
 import { changeMilestoneFlag } from "../../../services/changeMilestoneFlag";
@@ -77,105 +71,11 @@ const useEscrowDetailDialog = ({
     fetchRole();
   }, [selectedEscrow, userRoleInEscrow]);
 
-  const handleButtonClick = async (
-    selectedEscrow: Escrow,
-    milestone: Milestone,
-    index: number,
-  ) => {
-    switch (milestone.status) {
-      case "pending":
-        setIsChangingStatus(true);
-
-        await changeMilestoneStatus({
-          contractId: selectedEscrow?.contractId,
-          milestoneIndex: index.toString(),
-          newStatus: "completed",
-          serviceProvider: address,
-        });
-
-        const updatedMilestonesStatus = selectedEscrow.milestones.map(
-          (m, i): Milestone =>
-            i === index
-              ? {
-                  ...m,
-                  status: "completed" as MilestoneStatus,
-                }
-              : m,
-        );
-
-        const updatedPayloadStatus: EscrowPayload = {
-          ...selectedEscrow,
-          milestones: updatedMilestonesStatus,
-        };
-
-        const responseStatus = await updateEscrow({
-          escrowId: selectedEscrow.id,
-          payload: updatedPayloadStatus,
-        });
-
-        setIsChangingStatus(false);
-
-        if (responseStatus) {
-          handleClose();
-          toast({
-            title: "Success",
-            description: "The Milestone has been completed.",
-          });
-        }
-
-        break;
-      case "completed":
-        setIsChangingStatus(true);
-
-        await changeMilestoneFlag({
-          contractId: selectedEscrow?.contractId,
-          milestoneIndex: index.toString(),
-          newFlag: true,
-          client: address,
-        });
-
-        const updatedMilestonesFlag = selectedEscrow.milestones.map(
-          (m, i): Milestone =>
-            i === index
-              ? {
-                  ...m,
-                  flag: true,
-                }
-              : m,
-        );
-
-        const updatedPayloadFlag: EscrowPayload = {
-          ...selectedEscrow,
-          milestones: updatedMilestonesFlag,
-        };
-
-        const responseFlag = await updateEscrow({
-          escrowId: selectedEscrow.id,
-          payload: updatedPayloadFlag,
-        });
-
-        setIsChangingStatus(false);
-
-        if (responseFlag) {
-          handleClose();
-          toast({
-            title: "Success",
-            description: "The Milestone has been approved.",
-          });
-        }
-
-        break;
-      default:
-        console.log("No action available.");
-    }
-  };
-
   return {
     handleClose,
     areAllMilestonesCompleted,
     areAllMilestonesCompletedAndFlag,
     getFilteredStatusOptions,
-    handleButtonClick,
     userRoleInEscrow,
     role,
   };
