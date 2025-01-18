05b94b2fa93ed1710a71ec116f4c45c3b6a3712a
JoelVR17 <vermudo.com@gmail.com>
2025-01-18 17:58:21 -0600

    feat: enhance EntityCard component to display amount and fee details

--- src/components/modules/escrow/ui/dialogs/components/EntityCard.tsx
+++ src/components/modules/escrow/ui/dialogs/components/EntityCard.tsx
@@ -6,7 +6,9 @@ interface EntityCardProps {
   entity: string;
   type: string;
   hasPercentage?: boolean;
+  hasAmount?: boolean;
   percentage?: string;
+  amount?: string;
   inDispute?: boolean;
 }
 
@@ -14,30 +16,57 @@ const EntityCard = ({
   entity,
   type,
   hasPercentage,
+  hasAmount,
   percentage,
+  amount,
   inDispute,
 }: EntityCardProps) => {
-  const { formatAddress } = useFormatUtils();
+  const { formatAddress, formatDollar } = useFormatUtils();
 
   return (
     <div className={`flex flex-col w-full`}>
-      <h3 className="font-bold text-xs ml-2 mb-2 flex justify-between">
+      <div className="text-xs ml-2 mb-2 flex justify-between">
         <span className={`${inDispute && "text-destructive"}`}>{type}</span>
-        {hasPercentage && (
-          <span className="text-green-700 font-black">{percentage}%</span>
-        )}
-      </h3>
+
+        <div className="flex gap-3">
+          <div className="flex">
+            <p className="mr-1 font-bold">Fee:</p>
+            {hasPercentage && <span>{percentage}%</span>}
+          </div>
+          <div className="flex">
+            <p className="mr-1 font-bold">Amount:</p>
+            {hasAmount && <span>{formatDollar(amount)}</span>}
+          </div>
+        </div>
+      </div>
       <SidebarMenuButton
         size="lg"
         className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
       >
         <Avatar className="h-8 w-8 rounded-lg">
-          <AvatarImage src={""} alt={"name"} />
-          <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+          {type === "Trustless Work" ? (
+            <AvatarImage src={"/logo.png"} alt={"name"} />
+          ) : (
+            <>
+              <AvatarImage src={""} alt={"name"} />
+              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+            </>
+          )}
         </Avatar>
         <div className="grid flex-1 text-left text-sm leading-tight">
-          <span className="truncate text-xs font-semibold">{"name"}</span>
-          <span className="truncate text-xs">{formatAddress(entity)}</span>
+          {type === "Trustless Work" ? (
+            <>
+              <span className="truncate text-xs font-semibold">
+                Trustless Work
+              </span>
+              <span className="truncate text-xs">Private</span>
+            </>
+          ) : (
+            <>
+              <span className="truncate text-xs font-semibold">{"name"}</span>
+              <span className="truncate text-xs">{formatAddress(entity)}</span>
+            </>
+          )}
         </div>
       </SidebarMenuButton>
     </div>
