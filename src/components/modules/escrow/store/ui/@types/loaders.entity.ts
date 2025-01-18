f44e0269189d93f54cdc3c25b10fbd1c0c1335bd
JoelVR17 <vermudo.com@gmail.com>
2025-01-18 13:46:33 -0600

    feat: add funding escrow state and setter to loaders

--- src/components/modules/escrow/store/ui/@types/loaders.entity.ts
+++ src/components/modules/escrow/store/ui/@types/loaders.entity.ts
@@ -2,7 +2,9 @@ export interface LoadersEscrowStore {
   isChangingStatus: boolean;
   isDistributingEarnings: boolean;
   isStartingDispute: boolean;
+  isFundingEscrow: boolean;
   setIsChangingStatus: (value: boolean) => void;
   setIsDistributingEarnings: (value: boolean) => void;
   setIsStartingDispute: (value: boolean) => void;
+  setIsFundingEscrow: (value: boolean) => void;
 }
