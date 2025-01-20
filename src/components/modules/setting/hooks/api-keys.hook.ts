ef74fe8263cf401f82093a112b32e5eb8b52712e
Joel Vargas <vermudo.com@gmail.com>
2025-01-20 15:13:49 -0600

    feat: enhance useAPIKeys hook to include error handling and toast notifications for API key requests

--- src/components/modules/setting/hooks/api-keys.hook.ts
+++ src/components/modules/setting/hooks/api-keys.hook.ts
@@ -1,30 +1,41 @@
-import { UserPayload } from "@/@types/user.entity";
 import { useGlobalAuthenticationStore } from "@/core/store/data";
+import { useToast } from "@/hooks/use-toast";
 import { useChangeUtils } from "@/utils/hook/input-visibility.hook";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { requestApiKey } from "../services/requestApiKey";
 
 const useAPIKeys = () => {
   const [showApiKey, setShowApiKey] = useState("password");
   const { changeTypeInput } = useChangeUtils();
   const loggedUser = useGlobalAuthenticationStore((state) => state.loggedUser);
+  const address = useGlobalAuthenticationStore((state) => state.address);
+  const { toast } = useToast();
 
-  const form = useForm({
-    defaultValues: {
-      firstName: loggedUser?.firstName || "",
-      lastName: loggedUser?.lastName || "",
-    },
-  });
+  const onSubmit = async () => {
+    if (loggedUser?.useCase === "" || loggedUser?.useCase === null) {
+      toast({
+        title: "Error",
+        description: "You need to complete your use case first",
+        variant: "destructive",
+      });
+    }
 
-  const onSubmit = (data: UserPayload) => {
-    console.log(data);
+    const response = await requestApiKey(address);
+
+    if (!response) {
+      toast({
+        title: "Error",
+        description: "Error while requesting",
+        variant: "destructive",
+      });
+    }
   };
 
   const toggleVisibility = () => {
     changeTypeInput({ type: showApiKey, setType: setShowApiKey });
   };
 
-  return { form, onSubmit, showApiKey, toggleVisibility };
+  return { onSubmit, showApiKey, toggleVisibility };
 };
 
 export default useAPIKeys;
