80e1d609587899658407335eba5c96e34466a6c9
Joel Vargas <vermudo.com@gmail.com>
2025-01-21 14:37:24 -0600

    fix: remove project ID validation from Firestore initialization

--- src/core/config/firebase/firebase.ts
+++ src/core/config/firebase/firebase.ts
@@ -1,11 +1,4 @@
 import { getFirestore } from "firebase/firestore";
 import { firebaseApp } from "../../../../firebase";
 
-if (!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID) {
-  throw new Error("NEXT_PUBLIC_FIREBASE_PROJECT_ID is not defined");
-}
-
-export const db = getFirestore(
-  firebaseApp,
-  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-);
+export const db = getFirestore(firebaseApp);
