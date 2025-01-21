fd15a0a0f7d790464e060dec0c2470272c19c45d
Joel Vargas <vermudo.com@gmail.com>
2025-01-21 10:11:10 -0600

    feat: enhance useAPIKeys hook to include API key removal functionality and user data update

--- src/components/modules/setting/hooks/api-keys.hook.ts
+++ src/components/modules/setting/hooks/api-keys.hook.ts
@@ -1,14 +1,17 @@
 import { useGlobalAuthenticationStore } from "@/core/store/data";
 import { useToast } from "@/hooks/use-toast";
 import { useChangeUtils } from "@/utils/hook/input-visibility.hook";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { requestApiKey } from "../services/requestApiKey";
+import { getUser } from "../../auth/server/authentication-firebase";
+import { removeApiKey } from "../server/api-key-firebase";
 
 const useAPIKeys = () => {
   const [showApiKey, setShowApiKey] = useState("password");
   const { changeTypeInput } = useChangeUtils();
   const loggedUser = useGlobalAuthenticationStore((state) => state.loggedUser);
   const address = useGlobalAuthenticationStore((state) => state.address);
+  const updateUser = useGlobalAuthenticationStore((state) => state.updateUser);
   const { toast } = useToast();
 
   const onSubmit = async () => {
@@ -21,11 +24,38 @@ const useAPIKeys = () => {
     }
 
     const response = await requestApiKey(address);
+    const { data } = await getUser({ address });
+    await updateUser(address, data);
 
-    if (!response) {
+    if (response) {
+      toast({
+        title: "Success",
+        description: "Your API key has been generated",
+      });
+    } else {
       toast({
         title: "Error",
         description: "Error while requesting",
+        variant: "destructive",
+      });
+    }
+  };
+
+  const handleRemoveAPiKey = async (apiKey: string) => {
+    const { success } = await removeApiKey(address, apiKey);
+
+    if (success) {
+      toast({
+        title: "Success",
+        description: "API key removed",
+      });
+
+      const { data } = await getUser({ address });
+      updateUser(address, data);
+    } else {
+      toast({
+        title: "Error",
+        description: "Error while removing",
         variant: "destructive",
       });
     }
@@ -35,7 +65,7 @@ const useAPIKeys = () => {
     changeTypeInput({ type: showApiKey, setType: setShowApiKey });
   };
 
-  return { onSubmit, showApiKey, toggleVisibility };
+  return { onSubmit, showApiKey, toggleVisibility, handleRemoveAPiKey };
 };
 
 export default useAPIKeys;
