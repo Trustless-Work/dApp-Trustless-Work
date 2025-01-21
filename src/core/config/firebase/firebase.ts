eca86ee10cc3d8f8637099d19a0480a104e66169
Joel Vargas <vermudo.com@gmail.com>
2025-01-21 14:28:52 -0600

    fix: add validation for NEXT_PUBLIC_FIREBASE_PROJECT_ID before initializing Firestore

--- src/core/config/firebase/firebase.ts
+++ src/core/config/firebase/firebase.ts
@@ -1,4 +1,11 @@
 import { getFirestore } from "firebase/firestore";
 import { firebaseApp } from "../../../../firebase";
 
-export const db = getFirestore(firebaseApp);
+if (!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID) {
+  throw new Error("NEXT_PUBLIC_FIREBASE_PROJECT_ID is not defined");
+}
+
+export const db = getFirestore(
+  firebaseApp,
+  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+);
