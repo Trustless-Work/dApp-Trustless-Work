ac1bd0fcd4c83092518cfb7ccd09737ce9039e61
Joel Vargas <vermudo.com@gmail.com>
2025-01-21 11:58:57 -0600

    feat: add Firebase storage initialization for file management

--- firebase.ts
+++ firebase.ts
@@ -1,13 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 import { getFirestore } from "firebase/firestore/lite";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getStorage } from "firebase/storage";
 
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -21,3 +16,5 @@ const firebaseConfig = {
 export const firebaseApp = initializeApp(firebaseConfig);
 
 export const firebaseDB = getFirestore(firebaseApp);
+
+export const firebaseStorage = getStorage(firebaseApp);
