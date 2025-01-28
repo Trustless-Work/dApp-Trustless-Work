5ebe8559c9b3858c2624fa997a1c6eabd3b3be4d
Joel Vargas <vermudo.com@gmail.com>
2025-01-28 16:16:11 -0600

    feat: implement steps state management with useEscrowStepsSlice for improved step tracking

--- src/components/modules/escrow/store/ui/index.ts
+++ src/components/modules/escrow/store/ui/index.ts
@@ -10,12 +10,15 @@ import { useEscrowLoadersSlice } from "./slices/loaders.slice";
 import { LoadersEscrowStore } from "./@types/loaders.entity";
 import { useEscrowInitializeFormSlice } from "./slices/initialize-form.slice";
 import { InitializeFormEscrowStore } from "./@types/initialize-form.entity";
+import { useEscrowStepsSlice } from "./slices/steps.slice";
+import { StepsEscrowStore } from "./@types/steps.entity";
 
 type GlobalState = DialogEscrowStore &
   TabsEscrowStore &
   ViewModeEscrowStore &
   LoadersEscrowStore &
-  InitializeFormEscrowStore;
+  InitializeFormEscrowStore &
+  StepsEscrowStore;
 
 const devtoolsOptions: DevtoolsOptions = {
   name: "Global State",
@@ -53,6 +56,7 @@ export const useEscrowBoundedStore = create<GlobalState>()(
       ...useEscrowViewModeSlice(...a),
       ...useEscrowLoadersSlice(...a),
       ...useEscrowInitializeFormSlice(...a),
+      ...useEscrowStepsSlice(...a),
     }),
     devtoolsOptions,
   ),
