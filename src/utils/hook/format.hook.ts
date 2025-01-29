3b0148aed2df5c83a7148e57d43c325c18bb6af2
Joel Vargas <vermudo.com@gmail.com>
2025-01-29 13:48:10 -0600

    feat: enhance date formatting to include hours and minutes

--- src/utils/hook/format.hook.ts
+++ src/utils/hook/format.hook.ts
@@ -23,9 +23,11 @@ export const useFormatUtils = () => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
 
-    // DD/MM/YYYY
-    return `${day}/${month}/${year}`;
+    // DD/MM/YYYY HH:MM
+    return `${day}/${month}/${year} | ${hours}:${minutes}`;
   };
 
   const formatDollar = (amount: string | undefined | number): string => {
