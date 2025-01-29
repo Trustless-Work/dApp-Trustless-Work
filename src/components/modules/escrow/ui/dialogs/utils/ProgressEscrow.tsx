32ae1b1def78c1e3742571536f6eca58113c8a72
Joel Vargas <vermudo.com@gmail.com>
2025-01-29 16:15:27 -0600

    feat: update ProgressEscrow component to handle releaseFlag and improve milestone display logic

--- src/components/modules/escrow/ui/dialogs/utils/ProgressEscrow.tsx
+++ src/components/modules/escrow/ui/dialogs/utils/ProgressEscrow.tsx
@@ -6,86 +6,64 @@ interface ProgressEscrowProps {
 }
 
 const ProgressEscrow = ({ escrow }: ProgressEscrowProps) => {
+  // todo: use these constants in zusntand
+  const completedMilestones = escrow.milestones.filter(
+    (milestone) => milestone.status === "completed",
+  ).length;
+
+  const approvedMilestones = escrow.milestones.filter(
+    (milestone) => milestone.flag === true,
+  ).length;
+
+  const totalMilestones = escrow.milestones.length;
+
+  const progressPercentageCompleted =
+    totalMilestones > 0 ? (completedMilestones / totalMilestones) * 100 : 0;
+
+  const progressPercentageApproved =
+    totalMilestones > 0 ? (approvedMilestones / totalMilestones) * 100 : 0;
+
+  // Check if both are 100% and releaseFlag is false
+  const pendingRelease =
+    progressPercentageCompleted === 100 &&
+    progressPercentageApproved === 100 &&
+    !escrow.releaseFlag;
+
+  const isFinished =
+    progressPercentageCompleted === 100 && progressPercentageApproved === 100;
+
   return (
     <div className="flex flex-col gap-2 mt-4">
       <div className="flex w-full justify-between">
         {(() => {
-          const completedMilestones = escrow.milestones.filter(
-            (milestone) => milestone.status === "completed",
-          ).length;
-
-          const approvedMilestones = escrow.milestones.filter(
-            (milestone) => milestone.flag === true,
-          ).length;
-
-          const totalMilestones = escrow.milestones.length;
-
-          const progressPercentageCompleted =
-            totalMilestones > 0
-              ? (completedMilestones / totalMilestones) * 100
-              : 0;
-
-          const progressPercentageApproved =
-            totalMilestones > 0
-              ? (approvedMilestones / totalMilestones) * 100
-              : 0;
-
-          // Check if both are 100%
-          const isFinished =
-            progressPercentageCompleted === 100 &&
-            progressPercentageApproved === 100;
-
-          return isFinished ? (
-            <h3 className="mb-1 font-bold text-xs">Finished</h3>
-          ) : (
-            <>
-              <h3 className="mb-1 font-bold text-xs">Completed</h3>
-              <h3 className="mb-1 font-bold text-xs">Approved</h3>
-            </>
-          );
+          if (!isFinished || pendingRelease) {
+            return (
+              <>
+                <h3 className="mb-1 font-bold text-xs">Completed</h3>
+                <h3 className="mb-1 font-bold text-xs">Approved</h3>
+              </>
+            );
+          } else if (escrow.releaseFlag) {
+            return <h3 className="mb-1 font-bold text-xs h-8" />;
+          }
         })()}
       </div>
       <div className="flex items-center gap-2">
         {(() => {
-          const completedMilestones = escrow.milestones.filter(
-            (milestone) => milestone.status === "completed",
-          ).length;
-
-          const approvedMilestones = escrow.milestones.filter(
-            (milestone) => milestone.flag === true,
-          ).length;
-
-          const totalMilestones = escrow.milestones.length;
-
-          const progressPercentageCompleted =
-            totalMilestones > 0
-              ? (completedMilestones / totalMilestones) * 100
-              : 0;
-
-          const progressPercentageApproved =
-            totalMilestones > 0
-              ? (approvedMilestones / totalMilestones) * 100
-              : 0;
-
-          return (
-            <>
-              {progressPercentageCompleted === 100 &&
-              progressPercentageApproved === 100 ? (
-                <Progress value={100} />
-              ) : (
-                <>
-                  <strong className="text-xs">
-                    {progressPercentageCompleted.toFixed(0)}%
-                  </strong>
-                  <Progress value={progressPercentageCompleted} />
-                  <ProgressTwo value={progressPercentageApproved} />
-                  <strong className="text-xs">
-                    {progressPercentageApproved.toFixed(0)}%
-                  </strong>
-                </>
-              )}
-            </>
-          );
+          if (!isFinished || pendingRelease) {
+            return (
+              <>
+                <strong className="text-xs">
+                  {progressPercentageCompleted.toFixed(0)}%
+                </strong>
+                <Progress value={progressPercentageCompleted} />
+                <ProgressTwo value={progressPercentageApproved} />
+                <strong className="text-xs">
+                  {progressPercentageApproved.toFixed(0)}%
+                </strong>
+              </>
+            );
+          }
         })()}
       </div>
     </div>
