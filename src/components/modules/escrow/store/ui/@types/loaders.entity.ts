a6e6a83797ea123317c9e51de5f5a1058b4dd94c
Joel Vargas <vermudo.com@gmail.com>
2025-02-03 16:13:34 -0600

    feat: add editing milestones loader state to escrow store

--- src/components/modules/escrow/store/ui/@types/loaders.entity.ts
+++ src/components/modules/escrow/store/ui/@types/loaders.entity.ts
@@ -3,8 +3,10 @@ export interface LoadersEscrowStore {
   isDistributingEarnings: boolean;
   isStartingDispute: boolean;
   isFundingEscrow: boolean;
+  isEditingMilestones: boolean;
   setIsChangingStatus: (value: boolean) => void;
   setIsDistributingEarnings: (value: boolean) => void;
   setIsStartingDispute: (value: boolean) => void;
   setIsFundingEscrow: (value: boolean) => void;
+  setIsEditingMilestones: (value: boolean) => void;
 }
