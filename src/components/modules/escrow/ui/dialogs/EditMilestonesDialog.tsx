68376b675afbddc56135cd99c17e9b0034e7e866
Joel Vargas <vermudo.com@gmail.com>
2025-02-03 16:13:47 -0600

    feat: enhance EditMilestonesDialog with dynamic milestone management and improved form handling

--- src/components/modules/escrow/ui/dialogs/EditMilestonesDialog.tsx
+++ src/components/modules/escrow/ui/dialogs/EditMilestonesDialog.tsx
@@ -7,11 +7,14 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import SkeletonFundEscrow from "./utils/SkeletonFundEscrow";
-import { FormProvider } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import { useEscrowBoundedStore } from "../../store/ui";
 import { useGlobalBoundedStore } from "@/core/store/data";
 import useEditMilestonesDialog from "./hooks/edit-milestones-dialog.hook";
+import { Form, FormLabel } from "@/components/ui/form";
+import TooltipInfo from "@/components/utils/ui/Tooltip";
+import { Input } from "@/components/ui/input";
+import { Trash2 } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 interface FundEscrowDialogProps {
   isEditMilestoneDialogOpen: boolean;
@@ -22,10 +25,19 @@ const EditMilestonesDialog = ({
   isEditMilestoneDialogOpen,
   setIsEditMilestoneDialogOpen,
 }: FundEscrowDialogProps) => {
-  const { form, onSubmit, handleClose } = useEditMilestonesDialog({
+  const selectedEscrow = useGlobalBoundedStore((state) => state.selectedEscrow);
+  if (!selectedEscrow) return null;
+
+  const {
+    form,
+    onSubmit,
+    handleClose,
+    milestones,
+    handleAddMilestone,
+    handleRemoveMilestone,
+  } = useEditMilestonesDialog({
     setIsEditMilestoneDialogOpen,
   });
-  const selectedEscrow = useGlobalBoundedStore((state) => state.selectedEscrow);
 
   return (
     <Dialog open={isEditMilestoneDialogOpen} onOpenChange={handleClose}>
@@ -41,18 +53,78 @@ const EditMilestonesDialog = ({
         {false ? (
           <SkeletonFundEscrow />
         ) : (
-          <FormProvider {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="grid gap-4 py-4"
-            >
-              <div className="flex flex-col ms-center gap-4">milestones</div>
+          <Form {...form}>
+            <div className="grid gap-4 py-4">
+              <div className="flex flex-col ms-center gap-4">
+                <div className="space-y-4">
+                  <FormLabel className="flex items-center">
+                    Milestones
+                    <TooltipInfo content="Key stages or deliverables for the escrow." />
+                  </FormLabel>
+                  {milestones.map((milestone, index) => (
+                    <>
+                      <div key={index} className="flex items-center space-x-4">
+                        {milestone.flag ? (
+                          <Badge className="uppercase max-w-24">Approved</Badge>
+                        ) : (
+                          <Badge
+                            className="uppercase max-w-24"
+                            variant="outline"
+                          >
+                            {milestone.status}
+                          </Badge>
+                        )}
+
+                        <Input
+                          disabled={"approved_flag" in milestone}
+                          placeholder="Milestone Description"
+                          value={milestone.description}
+                          onChange={(e) => {
+                            const updatedMilestones = [...milestones];
+                            updatedMilestones[index].description =
+                              e.target.value;
+                            form.setValue("milestones", updatedMilestones);
+                          }}
+                        />
+
+                        <Button
+                          onClick={() => handleRemoveMilestone(index)}
+                          className="p-2 bg-transparent text-red-500 rounded-md border-none shadow-none hover:bg-transparent hover:shadow-none hover:text-red-500 focus:ring-0 active:ring-0"
+                          disabled={
+                            index === 0 ||
+                            milestone.status === "completed" ||
+                            milestone.flag
+                          }
+                        >
+                          <Trash2 className="h-5 w-5" />
+                        </Button>
+                      </div>
+
+                      {index === milestones.length - 1 && (
+                        <Button
+                          className="w-full md:w-1/4"
+                          variant="outline"
+                          onClick={handleAddMilestone}
+                          type="button"
+                        >
+                          Add Item
+                        </Button>
+                      )}
+                    </>
+                  ))}
+                </div>
+              </div>
 
               <DialogFooter>
-                <Button type="submit">Save</Button>
+                <Button
+                  type="button"
+                  onClick={() => onSubmit(form.getValues())}
+                >
+                  Save
+                </Button>
               </DialogFooter>
-            </form>
-          </FormProvider>
+            </div>
+          </Form>
         )}
       </DialogContent>
     </Dialog>
--- src/components/modules/escrow/ui/dialogs/hooks/edit-milestones-dialog.hook.ts
+++ src/components/modules/escrow/ui/dialogs/hooks/edit-milestones-dialog.hook.ts
@@ -5,11 +5,14 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { formSchema } from "../../../schema/fund-escrow.schema";
 import {
   useGlobalAuthenticationStore,
   useGlobalBoundedStore,
 } from "@/core/store/data";
+import { formSchema } from "../../../schema/edit-milestone.schema";
+import { Milestone } from "@/@types/escrow.entity";
+import { useEscrowBoundedStore } from "../../../store/ui";
+import { toast } from "@/hooks/toast.hook";
 
 interface useEditMilestonesDialogProps {
   setIsEditMilestoneDialogOpen: (value: boolean) => void;
@@ -20,26 +23,99 @@ const useEditMilestonesDialog = ({
 }: useEditMilestonesDialogProps) => {
   const { address } = useGlobalAuthenticationStore();
   const selectedEscrow = useGlobalBoundedStore((state) => state.selectedEscrow);
+  const setIsEditingMilestones = useEscrowBoundedStore(
+    (state) => state.setIsEditingMilestones,
+  );
+  const updateEscrow = useGlobalBoundedStore((state) => state.updateEscrow);
+  const fetchAllEscrows = useGlobalBoundedStore(
+    (state) => state.fetchAllEscrows,
+  );
+  const activeTab = useEscrowBoundedStore((state) => state.activeTab);
+  const setIsDialogOpen = useEscrowBoundedStore(
+    (state) => state.setIsDialogOpen,
+  );
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      amount: "",
+      milestones: selectedEscrow?.milestones || [{ description: "" }],
     },
   });
 
+  const milestones: Milestone[] = form.watch("milestones");
+
+  const handleAddMilestone = () => {
+    const currentMilestones = form.getValues("milestones");
+    const updatedMilestones = [
+      ...currentMilestones,
+      { description: "", status: "pending" },
+    ];
+    form.setValue("milestones", updatedMilestones);
+  };
+
+  const handleRemoveMilestone = (index: number) => {
+    const currentMilestones = form.getValues("milestones");
+    const updatedMilestones = currentMilestones.filter((_, i) => i !== index);
+    form.setValue("milestones", updatedMilestones);
+  };
+
   const onSubmit = async (payload: z.infer<typeof formSchema>) => {
-    // setIsFundingEscrow(true);
+    if (!selectedEscrow) return;
+
+    setIsEditingMilestones(true);
 
     try {
-    } catch (error) {}
+      const updatedEscrow = {
+        ...selectedEscrow,
+        milestones: payload.milestones,
+      };
+
+      const data = { status: "201" };
+      // const data = await updateEscrow({
+      //   escrowId: selectedEscrow.id,
+      //   payload: updatedEscrow,
+      // })
+
+      const response = await updateEscrow({
+        escrowId: selectedEscrow.id,
+        payload: updatedEscrow,
+      });
+
+      if (data.status === "SUCCESS" /*|| data.status === 201*/ && response) {
+        fetchAllEscrows({ address, type: activeTab || "client" });
+        setIsEditMilestoneDialogOpen(false);
+        setIsDialogOpen(false);
+
+        toast({
+          title: "Success",
+          description: `You have edited the milestones of ${selectedEscrow.title}.`,
+        });
+      }
+
+      setIsEditingMilestones(false);
+    } catch (error: any) {
+      setIsEditingMilestones(false);
+
+      toast({
+        title: "Error",
+        description: error.message,
+        variant: "destructive",
+      });
+    }
   };
 
   const handleClose = () => {
     setIsEditMilestoneDialogOpen(false);
   };
 
-  return { onSubmit, form, handleClose };
+  return {
+    onSubmit,
+    form,
+    handleClose,
+    milestones,
+    handleAddMilestone,
+    handleRemoveMilestone,
+  };
 };
 
 export default useEditMilestonesDialog;
