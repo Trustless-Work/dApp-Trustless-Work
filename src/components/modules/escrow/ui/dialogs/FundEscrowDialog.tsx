b2e58105303c96ef94dc2f8307faa22d3e4ae405
Joel Vargas <vermudo.com@gmail.com>
2025-02-03 14:04:54 -0600

    feat: replace LoaderData with SkeletonFundEscrow component in FundEscrowDialog for improved loading state

--- src/components/modules/escrow/ui/dialogs/FundEscrowDialog.tsx
+++ src/components/modules/escrow/ui/dialogs/FundEscrowDialog.tsx
@@ -21,6 +21,8 @@ import TooltipInfo from "@/components/utils/ui/Tooltip";
 import useFundEscrowDialogHook from "./hooks/fund-escrow-dialog.hook";
 import { useEscrowBoundedStore } from "../../store/ui";
 import LoaderData from "@/components/utils/ui/LoaderData";
+import { Skeleton } from "@/components/ui/skeleton";
+import SkeletonFundEscrow from "./utils/SkeletonFundEscrow";
 
 interface FundEscrowDialogProps {
   isSecondDialogOpen: boolean;
@@ -52,7 +54,7 @@ const FundEscrowDialog = ({
         </DialogHeader>
 
         {isFundingEscrow ? (
-          <LoaderData />
+          <SkeletonFundEscrow />
         ) : (
           <FormProvider {...form}>
             <form
