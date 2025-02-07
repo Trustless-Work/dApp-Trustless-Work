d3120ed358509978c0bb0c6397bad92f54d79a9c
JoelVR17 <vermudo.com@gmail.com>
2025-02-07 17:02:02 -0600

    feat: add loading state with SkeletonResolveDispute in ResolveDisputeEscrowDialog

--- src/components/modules/escrow/ui/dialogs/ResolveDisputeEscrowDialog.tsx
+++ src/components/modules/escrow/ui/dialogs/ResolveDisputeEscrowDialog.tsx
@@ -18,6 +18,8 @@ import {
 } from "@/components/ui/form";
 import TooltipInfo from "@/components/utils/ui/Tooltip";
 import useResolveDisputeEscrowDialogHook from "./hooks/resolve-dispute-escrow-dialog.hook";
+import SkeletonResolveDispute from "./utils/SkeletonResolveDispute";
+import { useEscrowBoundedStore } from "../../store/ui";
 
 interface ResolveDisputeEscrowDialogProps {
   isResolveDisputeDialogOpen: boolean;
@@ -31,6 +33,9 @@ const ResolveDisputeEscrowDialog = ({
   const { form, onSubmit, handleClose } = useResolveDisputeEscrowDialogHook({
     setIsResolveDisputeDialogOpen,
   });
+  const isResolvingDispute = useEscrowBoundedStore(
+    (state) => state.isResolvingDispute,
+  );
 
   return (
     <Dialog open={isResolveDisputeDialogOpen} onOpenChange={handleClose}>
@@ -42,64 +47,69 @@ const ResolveDisputeEscrowDialog = ({
             the two entities.
           </DialogDescription>
         </DialogHeader>
-        <FormProvider {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="grid gap-4 py-4"
-          >
-            <div className="flex flex-col ms-center gap-4">
-              <FormField
-                control={form.control}
-                name="clientFunds"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="flex items-center">
-                      Client Amount
-                      <TooltipInfo content="The amount for the client." />
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="The amount for the client"
-                        {...field}
-                        onChange={(e) => {
-                          field.onChange(e);
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
 
-              <FormField
-                control={form.control}
-                name="serviceProviderFunds"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="flex items-center">
-                      Service Provider Amount
-                      <TooltipInfo content="The amount for the service provider." />
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="The amount for the service provider"
-                        {...field}
-                        onChange={(e) => {
-                          field.onChange(e);
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
+        {isResolvingDispute ? (
+          <SkeletonResolveDispute />
+        ) : (
+          <FormProvider {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid gap-4 py-4"
+            >
+              <div className="flex flex-col ms-center gap-4">
+                <FormField
+                  control={form.control}
+                  name="clientFunds"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="flex items-center">
+                        Client Amount
+                        <TooltipInfo content="The amount for the client." />
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="The amount for the client"
+                          {...field}
+                          onChange={(e) => {
+                            field.onChange(e);
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
 
-            <DialogFooter>
-              <Button type="submit">Resolve Conflicts</Button>
-            </DialogFooter>
-          </form>
-        </FormProvider>
+                <FormField
+                  control={form.control}
+                  name="serviceProviderFunds"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="flex items-center">
+                        Service Provider Amount
+                        <TooltipInfo content="The amount for the service provider." />
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="The amount for the service provider"
+                          {...field}
+                          onChange={(e) => {
+                            field.onChange(e);
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <DialogFooter>
+                <Button type="submit">Resolve Conflicts</Button>
+              </DialogFooter>
+            </form>
+          </FormProvider>
+        )}
       </DialogContent>
     </Dialog>
   );
