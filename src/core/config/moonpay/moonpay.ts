7600151b175746042a81969178905cbe7bd14be5
JoelVR17 <vermudo.com@gmail.com>
2025-02-12 11:26:20 -0600

    chore: comment out unused authentication store and wallet address in MoonPay configuration

--- src/core/config/moonpay/moonpay.ts
+++ src/core/config/moonpay/moonpay.ts
@@ -1,4 +1,4 @@
-import { useGlobalAuthenticationStore } from "@/core/store/data";
+// import { useGlobalAuthenticationStore } from "@/core/store/data";
 
 const MOONPAY_CONFIG = {
   apiKey: process.env.NEXT_PUBLIC_MOONPAY_API_KEY || "",
@@ -10,12 +10,12 @@ export const useGenerateMoonPayUrl = async (
   contractId: string,
   amount: number,
 ) => {
-  const address = useGlobalAuthenticationStore((state) => state.address);
+  //const address = useGlobalAuthenticationStore((state) => state.address);
 
   const params = new URLSearchParams({
     apiKey: MOONPAY_CONFIG.apiKey,
     currencyCode: MOONPAY_CONFIG.currency,
-    walletAddress: address,
+    //walletAddress: address,
     baseCurrencyAmount: amount.toString(),
     redirectURL: `${window.location.origin}/dashboard/escrow/fund-escrow/success?contractId=${contractId}`,
     failureRedirectURL: `${window.location.origin}/dashboard/escrow/fund-escrow/failure`,
