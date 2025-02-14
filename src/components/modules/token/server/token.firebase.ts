e5ccb127fb238ec045bd484ce58579377d98c92e
Joel Vargas <vermudo.com@gmail.com>
2025-02-14 12:34:47 -0600

    feat: implement getAllTokens function to retrieve tokens from Firestore

---
+++ src/components/modules/token/server/token.firebase.ts
@@ -0,0 +1,50 @@
+import { db } from "@/core/config/firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+const getAllTokens = async (): Promise<{
+  success: boolean;
+  message: string;
+  data?: any[];
+}> => {
+  const collectionRef = collection(db, "tokens");
+
+  try {
+    const querySnapshot = await getDocs(collectionRef);
+
+    if (querySnapshot.empty) {
+      return {
+        success: false,
+        message: "No tokens found",
+      };
+    }
+
+    const tokens = querySnapshot.docs
+      .map((doc) => {
+        const tokenData = doc.data();
+        if (!tokenData.name || !tokenData.token) {
+          return null;
+        }
+
+        return {
+          id: doc.id,
+          ...tokenData,
+        };
+      })
+      .filter((token) => token !== null);
+
+    return {
+      success: true,
+      message: "Tokens retrieved successfully",
+      data: tokens,
+    };
+  } catch (error: any) {
+    const errorMessage =
+      error.response && error.response.data
+        ? error.response.data.message
+        : "An error occurred";
+
+    return { success: false, message: errorMessage };
+  }
+};
+
+export { getAllTokens };
