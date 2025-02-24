8584e741b0fb21d99f3523d26cddb399bdb926c8
JoelVR17 <vermudo.com@gmail.com>
2025-02-23 23:31:40 -0600

    feat: add optional id prop to CreateButton component

--- src/components/utils/ui/Create.tsx
+++ src/components/utils/ui/Create.tsx
@@ -5,11 +5,12 @@ interface CreateButtonProps {
   label: string;
   url: string;
   className?: string;
+  id?: string;
 }
 
-const CreateButton = ({ label, url, className }: CreateButtonProps) => {
+const CreateButton = ({ label, url, className, id }: CreateButtonProps) => {
   return (
-    <Link href={url}>
+    <Link href={url} id={id || ""}>
       <Button className={className}>{label}</Button>
     </Link>
   );
