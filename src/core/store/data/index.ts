01bd6a096c2ac516ca972325d4971e1b2be8343e
Joel Vargas <vermudo.com@gmail.com>
2025-03-10 13:17:42 -0600

    feat: replace TokenGlobalStore with TrustlineGlobalStore and implement trustline slice

--- src/core/store/data/index.ts
+++ src/core/store/data/index.ts
@@ -4,10 +4,10 @@ import { create } from "zustand";
 import { useGlobalEscrowsSlice } from "./slices/escrows.slice";
 import { useGlobalAuthenticationSlice } from "./slices/authentication.slice";
 import { AuthenticationGlobalStore } from "./@types/authentication.entity";
-import { useGlobalTokensSlice } from "./slices/tokens.slice";
-import { TokenGlobalStore } from "./@types/tokens.entity";
+import { TrustlineGlobalStore } from "./@types/trustlines.entity";
+import { useGlobalTrustlinesSlice } from "./slices/trustlines.slice";
 
-type GlobalState = EscrowGlobalStore & TokenGlobalStore;
+type GlobalState = EscrowGlobalStore & TrustlineGlobalStore;
 type AuthState = AuthenticationGlobalStore;
 
 const devtoolsOptions: DevtoolsOptions = {
@@ -42,7 +42,7 @@ export const useGlobalBoundedStore = create<GlobalState>()(
   devtools(
     (...a) => ({
       ...useGlobalEscrowsSlice(...a),
-      ...useGlobalTokensSlice(...a),
+      ...useGlobalTrustlinesSlice(...a),
     }),
     devtoolsOptions,
   ),
