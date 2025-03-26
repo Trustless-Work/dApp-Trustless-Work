f7d0dd7bb55d8d2d3cc8923362d70d2d1c235d5e
JoelVR17 <vermudo.com@gmail.com>
2025-03-26 17:05:16 -0600

    feat: enhance middleware to handle maintenance mode redirection

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const maintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
 
+  // Maintainance mode activated
   if (
     maintenanceMode &&
     request.nextUrl.pathname !== "/maintenance" &&
@@ -11,6 +12,11 @@ export function middleware(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/static")
   ) {
     return NextResponse.redirect(new URL("/maintenance", request.url));
+  }
+
+  // Maintainance mode deactivated
+  if (!maintenanceMode && request.nextUrl.pathname === "/maintenance") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
