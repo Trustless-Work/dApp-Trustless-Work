c90ee583a319eacccefa552aab094ba4bba0409a
JoelVR17 <vermudo.com@gmail.com>
2025-04-03 14:25:52 -0600

    feat: enhance profile image handling in useProfile hook

--- src/components/modules/setting/hooks/profile-section.hook.ts
+++ src/components/modules/setting/hooks/profile-section.hook.ts
@@ -39,13 +39,12 @@ const useProfile = ({ onSave }: useProfileProps) => {
   });
 
   const onSubmit = (data: UserPayload) => {
-    onSave(data);
+    onSave({ ...data, profileImage: form.getValues("profileImage") });
   };
 
   const handleProfileImageUpload = async (file: File) => {
     try {
       const allowedExtensions = ["jpg", "jpeg", "png", "webp", "svg"];
-
       const fileExtension = file.name.split(".").pop()?.toLowerCase();
 
       if (!fileExtension || !allowedExtensions.includes(fileExtension)) {
@@ -58,12 +57,11 @@ const useProfile = ({ onSave }: useProfileProps) => {
       }
 
       const storageRef = ref(firebaseStorage, `users/${v4()}.${fileExtension}`);
-
       await uploadBytes(storageRef, file);
-
       const url = await getDownloadURL(storageRef);
 
       await updateUser(address, { ...loggedUser, profileImage: url });
+      form.setValue("profileImage", url, { shouldDirty: true });
 
       toast({
         title: "Success",
@@ -92,8 +90,8 @@ const useProfile = ({ onSave }: useProfileProps) => {
 
       const storageRef = ref(firebaseStorage, loggedUser.profileImage);
       await deleteObject(storageRef);
-
       await updateUser(address, { ...loggedUser, profileImage: "" });
+      form.setValue("profileImage", "", { shouldDirty: true });
 
       toast({
         title: "Success",
