d053dd11318aab1b0f2a69d46ec2c8fbf02cfa9a
Joel Vargas <vermudo.com@gmail.com>
2025-04-08 16:14:21 -0600

    refactor: remove unused sendTransaction function and related imports

--- src/lib/stellar-wallet-kit.ts
+++ src/lib/stellar-wallet-kit.ts
@@ -1,24 +1,5 @@
 import { kit } from "@/components/modules/auth/wallet/constants/wallet-kit.constant";
-import http from "@/core/config/axios/http";
 import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
-import { AxiosResponse } from "axios";
-
-interface sendTransactionProps {
-  signedXdr: string;
-  returnEscrowDataIsRequired: boolean;
-}
-
-export const sendTransaction = async ({
-  signedXdr,
-  returnEscrowDataIsRequired,
-}: sendTransactionProps): Promise<AxiosResponse> => {
-  const tx = await http.post("/helper/send-transaction", {
-    signedXdr,
-    returnEscrowDataIsRequired: returnEscrowDataIsRequired,
-  });
-
-  return tx;
-};
 
 interface signTransactionProps {
   unsignedTransaction: string;
