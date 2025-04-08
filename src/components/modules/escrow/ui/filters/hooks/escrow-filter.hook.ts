f0defbe501a171a9013a9964015663ad2330d6c3
Joel Vargas <vermudo.com@gmail.com>
2025-04-08 17:20:21 -0600

    feat: implement escrow filter hook for search, status, and amount range

---
+++ src/components/modules/escrow/ui/filters/hooks/escrow-filter.hook.ts
@@ -0,0 +1,80 @@
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export const useEscrowFilter = () => {
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams.toString());
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [search, setSearch] = useState(searchParams.get("q") || "");
+  const [status, setStatus] = useState(searchParams.get("status") || "");
+  const [amountRange, setAmountRange] = useState(
+    searchParams.get("amount") || "",
+  );
+
+  const updateQuery = (key: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set(key, value);
+    } else {
+      params.delete(key);
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  };
+
+  const deleteParams = () => {
+    params.delete("q");
+    params.delete("status");
+    params.delete("amount");
+    router.replace(`${pathname}?${params.toString()}`);
+  };
+
+  useEffect(() => {
+    const delayDebounce = setTimeout(() => {
+      updateQuery("q", search);
+    }, 500);
+    return () => clearTimeout(delayDebounce);
+  }, [search]);
+
+  const mapNameParams = (paramName: string) => {
+    if (!paramName) return "All Statuses";
+
+    switch (paramName) {
+      // Statuses
+      case "all":
+        return "All Statuses";
+      case "released":
+        return "Released";
+      case "resolved":
+        return "Resolved";
+      case "inDispute":
+        return "In Dispute";
+
+      // Amounts
+      case "0-100":
+        return "$0 - $100";
+      case "100-500":
+        return "$100 - $500";
+      case "500-1000":
+        return "$500 - $1000";
+      case "1000+":
+        return "Over $1000";
+    }
+  };
+
+  return {
+    search,
+    router,
+    status,
+    amountRange,
+    pathname,
+    searchParams,
+    setStatus,
+    setSearch,
+    setAmountRange,
+    updateQuery,
+    deleteParams,
+    mapNameParams,
+  };
+};
