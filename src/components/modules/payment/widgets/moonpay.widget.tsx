f148ea2033f416e9470d2d633879995f924a376a
Joel Vargas <vermudo.com@gmail.com>
2025-04-08 10:15:43 -0600

    update: adjust MoonpayWidget to correct currency settings and improve wallet visibility instructions

--- src/components/modules/payment/widgets/moonpay.widget.tsx
+++ src/components/modules/payment/widgets/moonpay.widget.tsx
@@ -19,14 +19,19 @@ export const MoonpayWidget = ({ visible, wallet }: MoonpayWidgetProps) => {
 
   return (
     <MoonPayBuyWidget
+      // ! In order for the wallet to appear correctly, the wallet and these 2 properties must match in the same chain, for exmaple:
+      // defaultCurrencyCode="eth"
+      // showOnlyCurrencies="eth"
+      // walletAddress="0x59bFeA666108ab2c7f85485A8029d87A3D0DAcB2"
+
       variant="overlay"
       baseCurrencyCode="usd"
       baseCurrencyAmount={amountMoonpay}
       visible={visible}
       redirectURL="https://moonpay.com/"
       walletAddress={wallet}
-      showOnlyCurrencies="btc,eth,usdt,usdc,sol"
-      //showOnlyCurrencies="usdc_xlm"
+      defaultCurrencyCode="usdc_xlm"
+      showOnlyCurrencies="usdc_xlm"
       onClose={() => {
         setIsMoonpayWidgetOpen(false);
         return Promise.resolve();
