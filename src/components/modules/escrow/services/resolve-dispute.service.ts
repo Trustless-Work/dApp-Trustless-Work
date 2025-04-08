2089cde7c4676c72eed0cf7f231b29dfc0014fbd
Joel Vargas <vermudo.com@gmail.com>
2025-04-08 13:36:57 -0600

    update: refactor resolveDispute service to enhance transaction signing process

--- src/components/modules/escrow/services/resolve-dispute.service.ts
+++ src/components/modules/escrow/services/resolve-dispute.service.ts
@@ -1,26 +1,23 @@
 import { ResolveDisputePayload } from "@/@types/escrow.entity";
 import http from "@/core/config/axios/http";
 import { kit } from "@/components/modules/auth/wallet/constants/wallet-kit.constant";
-import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
-import { signTransaction } from "@stellar/freighter-api";
 import axios from "axios";
+import { sendTransaction, signTransaction } from "@/lib/stellar-wallet-kit";
 
 export const resolveDispute = async (payload: ResolveDisputePayload) => {
   try {
-    const response = await http.post("/escrow/resolving-disputes", payload);
-
-    const { unsignedTransaction } = response.data;
     const { address } = await kit.getAddress();
-    const { signedTxXdr } = await signTransaction(unsignedTransaction, {
-      address,
-      networkPassphrase: WalletNetwork.TESTNET,
-    });
 
-    const tx = await http.post("/helper/send-transaction", {
+    const response = await http.post("/escrow/resolving-disputes", payload);
+    const { unsignedTransaction } = response.data;
+
+    const signedTxXdr = await signTransaction({ unsignedTransaction, address });
+
+    const { data } = await sendTransaction({
       signedXdr: signedTxXdr,
+      returnEscrowDataIsRequired: true,
     });
 
-    const { data } = tx;
     return data;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
