449f29588fc220ed013e77ecbee6cd9a68b65718
Joel Vargas <vermudo.com@gmail.com>
2025-04-08 13:36:50 -0600

    update: refactor editMilestones service to optimize transaction signing process

--- src/components/modules/escrow/services/edit-milestones.service.ts
+++ src/components/modules/escrow/services/edit-milestones.service.ts
@@ -1,29 +1,26 @@
 import { EditMilestonesPayload } from "@/@types/escrow.entity";
 import http from "@/core/config/axios/http";
 import { kit } from "@/components/modules/auth/wallet/constants/wallet-kit.constant";
-import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
-import { signTransaction } from "@stellar/freighter-api";
 import axios from "axios";
+import { sendTransaction, signTransaction } from "@/lib/stellar-wallet-kit";
 
 export const editMilestones = async (payload: EditMilestonesPayload) => {
   try {
+    const { address } = await kit.getAddress();
+
     const response = await http.put(
       "/escrow/update-escrow-by-contract-id",
       payload,
     );
-
     const { unsignedTransaction } = response.data;
-    const { address } = await kit.getAddress();
-    const { signedTxXdr } = await signTransaction(unsignedTransaction, {
-      address,
-      networkPassphrase: WalletNetwork.TESTNET,
-    });
 
-    const tx = await http.post("/helper/send-transaction", {
+    const signedTxXdr = await signTransaction({ unsignedTransaction, address });
+
+    const { data } = await sendTransaction({
       signedXdr: signedTxXdr,
+      returnEscrowDataIsRequired: true,
     });
 
-    const { data } = tx;
     return data;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
