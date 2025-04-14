aad507f20105659109ee5cc6fb49602a195ad9a8
Joel Vargas <vermudo.com@gmail.com>
2025-04-14 15:29:11 -0600

    feat: enhance MetricCard and MetricsSection with loading state and new metrics

--- src/components/modules/dashboard/ui/cards/MetricCard.tsx
+++ src/components/modules/dashboard/ui/cards/MetricCard.tsx
@@ -1,12 +1,14 @@
 import { cn } from "@/lib/utils";
 import { Card, CardContent } from "../../../../ui/card";
+import { SkeletonMetricCard } from "../utils/SkeletonMetricCard";
 
 interface MetricCardProps {
   title: string;
   value: string | number;
   icon?: React.ReactNode;
-  subValue?: string;
+  subValue?: string | React.ReactNode;
   className?: string;
+  isLoading?: boolean;
 }
 
 const MetricCard = ({
@@ -15,19 +17,28 @@ const MetricCard = ({
   icon,
   subValue,
   className,
+  isLoading = false,
 }: MetricCardProps) => {
   return (
     <Card className={cn("overflow-hidden", className)}>
       <CardContent className="p-6">
-        <div className="flex items-center justify-between">
-          <p className="text-sm font-medium text-muted-foreground">{title}</p>
-          {icon && <div className="text-muted-foreground">{icon}</div>}
-        </div>
-        <div className="mt-2 flex items-baseline">
-          <h3 className="text-2xl font-semibold">{value}</h3>
-        </div>
-        {subValue && (
-          <p className="mt-2 text-sm text-muted-foreground">{subValue}</p>
+        {isLoading ? (
+          <SkeletonMetricCard />
+        ) : (
+          <>
+            <div className="flex items-center justify-between">
+              <p className="text-sm font-medium text-muted-foreground">
+                {title}
+              </p>
+              {icon && <div className="text-muted-foreground">{icon}</div>}
+            </div>
+            <div className="mt-2 flex items-baseline">
+              <h3 className="text-2xl font-semibold">{value}</h3>
+            </div>
+            {subValue && (
+              <p className="mt-2 text-sm text-muted-foreground">{subValue}</p>
+            )}
+          </>
         )}
       </CardContent>
     </Card>
--- src/components/modules/dashboard/ui/cards/MetricSection.tsx
+++ src/components/modules/dashboard/ui/cards/MetricSection.tsx
@@ -1,35 +1,85 @@
-import { DollarSign, Users, CreditCard, Activity } from "lucide-react";
+import {
+  Layers,
+  Handshake,
+  ArrowUpRight,
+  ArrowDownRight,
+  TrendingUpDown,
+  Hand,
+  CircleCheckBig,
+  Ban,
+} from "lucide-react";
 import MetricCard from "./MetricCard";
-import { useFormatUtils } from "@/utils/hook/format.hook";
+import { useEscrowDashboardData } from "../../hooks/use-escrow-dashboard-data.hook";
+import { useGlobalAuthenticationStore } from "@/core/store/data";
 
 const MetricsSection = () => {
-  const { formatDollar, formatPercentage, formatNumber } = useFormatUtils();
+  const address = useGlobalAuthenticationStore((state) => state.address);
+  const data = useEscrowDashboardData({ address });
+
+  const {
+    totalEscrows = 0,
+    totalResolved = 0,
+    totalReleased = 0,
+    totalInDispute = 0,
+    resolvedPercentage = 0,
+    isPositive = false,
+  } = data || {};
 
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       <MetricCard
-        title="Total Revenue"
-        value={formatDollar("45231.8")}
-        subValue={`${formatPercentage(20.1)} from last month`}
-        icon={<DollarSign className="h-4 w-4" />}
+        title="Total Escrows"
+        value={String(totalEscrows).padStart(2, "0")}
+        subValue="All time escrow count"
+        icon={<Layers className="h-4 w-4" />}
+        isLoading={!data}
       />
       <MetricCard
-        title="Subscriptions"
-        value={formatNumber(2350)}
-        subValue={`${formatPercentage(180.1)} from last month`}
-        icon={<Users className="h-4 w-4" />}
+        title="Resolved Escrows"
+        value={String(totalResolved).padStart(2, "0")}
+        subValue="Disputes successfully resolved"
+        icon={<Handshake className="h-4 w-4" />}
+        isLoading={!data}
       />
       <MetricCard
-        title="Sales"
-        value={formatNumber(12234)}
-        subValue={`${formatPercentage(19)} from last month`}
-        icon={<CreditCard className="h-4 w-4" />}
+        title="Released Escrows"
+        value={String(totalReleased).padStart(2, "0")}
+        subValue="Successfully released"
+        icon={<CircleCheckBig className="h-4 w-4" />}
+        isLoading={!data}
       />
       <MetricCard
-        title="Active Now"
-        value={formatNumber(573)}
-        subValue={`${formatPercentage(201)} since last hour`}
-        icon={<Activity className="h-4 w-4" />}
+        title="Working Escrows"
+        value={String(totalEscrows - totalResolved).padStart(2, "0")}
+        subValue="Awaiting for milestone completion"
+        icon={<Hand className="h-4 w-4" />}
+        isLoading={!data}
+      />
+      <MetricCard
+        title="In Dispute Escrows"
+        value={String(totalInDispute).padStart(2, "0")}
+        subValue="Escrows in dispute"
+        icon={<Ban className="h-4 w-4" />}
+        isLoading={!data}
+      />
+      <MetricCard
+        title="Resolution Rate"
+        value={resolvedPercentage + "%"}
+        subValue={
+          <div className="flex items-center gap-1">
+            <p className={`text-sm ${isPositive && "text-green-500"}`}>
+              {isPositive ? "Good" : "Needs improvement"}
+            </p>
+
+            {isPositive ? (
+              <ArrowUpRight className="h-4 w-4 text-green-500" />
+            ) : (
+              <ArrowDownRight className="h-4 w-4 text-destructive" />
+            )}
+          </div>
+        }
+        icon={<TrendingUpDown className="h-4 w-4" />}
+        isLoading={!data}
       />
     </div>
   );
