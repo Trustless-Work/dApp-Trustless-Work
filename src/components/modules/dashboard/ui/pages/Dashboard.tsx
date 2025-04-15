73596a3b0df96f0f9c95caad539f71158472b1c3
Joel Vargas <vermudo.com@gmail.com>
2025-04-15 10:51:54 -0600

    feat: enhance Dashboard component with empty state handling and create escrow button

--- src/components/modules/dashboard/ui/pages/Dashboard.tsx
+++ src/components/modules/dashboard/ui/pages/Dashboard.tsx
@@ -10,6 +10,8 @@ import MetricsSection from "../cards/MetricSection";
 import { SkeletonEscrowVolumeTrendChart } from "../utils/SkeletonEscrowVolumeTrendChart";
 import { SkeletonEscrowStatusChart } from "../utils/SkeletonStatusChart";
 import { SkeletonEscrowReleaseTrendChart } from "../utils/SkeletonEscrowReleaseTrendChart";
+import { ArrowRight } from "lucide-react";
+import CreateButton from "@/components/utils/ui/Create";
 
 export default function Dashboard() {
   const address = useGlobalAuthenticationStore((state) => state.address);
@@ -17,41 +19,58 @@ export default function Dashboard() {
 
   const { statusCounts, releaseTrend, volumeTrend, top5ByValue } = data || {};
 
+  const hasData = data && data.totalEscrows > 0;
+
   return (
-    <div className="flex flex-col w-full h-full gap-4">
-      <MetricsSection />
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-6">
-        <div className="md:col-span-2">
-          {data?.volumeTrend ? (
-            <EscrowVolumeTrendChart data={volumeTrend || []} />
-          ) : (
-            <SkeletonEscrowVolumeTrendChart />
-          )}
+    <>
+      {!hasData && (
+        <div className="flex items-center justify-end w-full gap-2">
+          <span className="text-sm flex items-center text-muted-foreground mr-2">
+            Don't have any escrow? <ArrowRight className="ml-2" />
+          </span>
+          <CreateButton
+            className="mr-auto w-full md:w-auto"
+            label="Create Escrow"
+            url={"/dashboard/escrow/initialize-escrow"}
+          />
         </div>
+      )}
 
-        <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-10 gap-4">
-          <div className="md:col-span-3">
-            {data?.statusCounts ? (
-              <EscrowStatusChart data={statusCounts || []} />
+      <div className="flex flex-col w-full h-full gap-4">
+        <MetricsSection />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-6">
+          <div className="md:col-span-2">
+            {data?.volumeTrend ? (
+              <EscrowVolumeTrendChart data={volumeTrend || []} />
             ) : (
-              <SkeletonEscrowStatusChart />
+              <SkeletonEscrowVolumeTrendChart />
             )}
           </div>
 
-          <div className="md:col-span-7">
-            {data?.releaseTrend ? (
-              <EscrowReleaseTrendChart data={releaseTrend || []} />
-            ) : (
-              <SkeletonEscrowReleaseTrendChart />
-            )}
-          </div>
-        </div>
+          <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-10 gap-4">
+            <div className="md:col-span-3">
+              {data?.statusCounts ? (
+                <EscrowStatusChart data={statusCounts || []} />
+              ) : (
+                <SkeletonEscrowStatusChart />
+              )}
+            </div>
 
-        <div className="md:col-span-2">
-          <TopEscrowsList escrows={top5ByValue || []} />
+            <div className="md:col-span-7">
+              {data?.releaseTrend ? (
+                <EscrowReleaseTrendChart data={releaseTrend || []} />
+              ) : (
+                <SkeletonEscrowReleaseTrendChart />
+              )}
+            </div>
+          </div>
+
+          <div className="md:col-span-2">
+            <TopEscrowsList escrows={top5ByValue || []} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
