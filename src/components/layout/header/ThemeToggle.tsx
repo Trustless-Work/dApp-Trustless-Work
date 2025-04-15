906c6ffc0fd7ebdbb442beb9d4109ac219314dca
Joel Vargas <vermudo.com@gmail.com>
2025-04-15 12:19:27 -0600

    fix: update accessibility text and labels for theme toggle options in ThemeToggle component

--- src/components/layout/header/ThemeToggle.tsx
+++ src/components/layout/header/ThemeToggle.tsx
@@ -32,15 +32,23 @@ const ThemeToggle = () => {
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Cambiar tema</span>
+          <span className="sr-only">Toggle Theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => theme !== "light" && toggleTheme()}>
-          Claro
+        <DropdownMenuItem
+          onClick={() => theme !== "light" && toggleTheme()}
+          className="flex gap-2 cursor-pointer"
+        >
+          <Sun />
+          Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => theme !== "dark" && toggleTheme()}>
-          Oscuro
+        <DropdownMenuItem
+          onClick={() => theme !== "dark" && toggleTheme()}
+          className="flex gap-2 cursor-pointer"
+        >
+          <Moon />
+          Dark
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
