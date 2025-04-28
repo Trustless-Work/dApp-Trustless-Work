d3e4953e0c1560c1391dd333205b51f15a2605bf
Joel Vargas <vermudo.com@gmail.com>
2025-04-28 09:40:21 -0600

    fix: adjust grid column layout in SkeletonCards component for better responsiveness

--- src/components/modules/escrow/ui/utils/SkeletonCards.tsx
+++ src/components/modules/escrow/ui/utils/SkeletonCards.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SkeletonCards = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
       {Array.from({ length: 8 }).map((_, index) => (
         <div key={index} className="flex flex-col gap-4 mb-2">
           <Skeleton className="h-44 w-full rounded-xl" />
