038b796511d6b0ced487b7b51587eac7f9c8ee6c
Joel Vargas <vermudo.com@gmail.com>
2025-05-05 13:48:05 -0600

    chore: remove unused notification collections from Firebase configuration

--- src/core/config/firebase/firebase.ts
+++ src/core/config/firebase/firebase.ts
@@ -1,9 +1,4 @@
-import { getFirestore, collection } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { firebaseApp } from "../../../../firebase";
 
 export const db = getFirestore(firebaseApp);
-
-// Notification collections
-export const notificationsCollection = collection(db, "notifications");
-export const userNotificationsCollection = (walletAddress: string) =>
-  collection(db, "users", walletAddress, "notifications");
