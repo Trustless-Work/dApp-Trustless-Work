ccfbbdb3e2c59edb95b489a36256f0c5a96e52d0
Joel Vargas <vermudo.com@gmail.com>
2025-05-07 16:19:13 -0600

    refactor: remove old escrow entity types and introduce new escrow payload and response structures

---
+++ src/@types/escrows/escrow.entity.ts
@@ -0,0 +1,39 @@
+import { Trustline } from "../trustline.entity";
+
+export type Milestone = {
+  description: string;
+  status: string;
+  evidence: string;
+  approvedFlag: boolean;
+};
+
+export type Roles = {
+  approver: string;
+  serviceProvider: string;
+  platformAddress: string;
+  releaseSigner: string;
+  disputeResolver: string;
+  receiver: string;
+};
+
+export type Flags = {
+  disputeFlag?: boolean;
+  releaseFlag?: boolean;
+  resolvedFlag?: boolean;
+};
+
+export interface Escrow {
+  signer?: string;
+  contractId?: string;
+  engagementId: string;
+  title: string;
+  roles: Roles;
+  description: string;
+  amount: string;
+  platformFee: string;
+  balance?: string;
+  milestones: Milestone[];
+  flags?: Flags;
+  trustline: Trustline;
+  receiverMemo: number;
+}
