f407c82973c3107a90c6bf1b65a467abe1b383d7
William Ugalde Gamboa <wugalde19@gmail.com>
2025-06-07 15:01:16 -0600

    feat: replace SettingsNavigationMenu with new SettingsSidebar component

--- src/components/modules/setting/Settings.tsx
+++ src/components/modules/setting/Settings.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/layout/header/Header";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import SettingsNavigationMenu from "./SettingsNavigationMenu";
 import useSettings from "./hooks/settings.hook";
 import { Profile } from "./ui/sections/Profile";
 import { Appearance } from "./ui/sections/Appearance";
 import { Preferences } from "./ui/sections/Preferences";
 import { APIKeys } from "./ui/sections/APIKeys";
+import SettingsSidebar from "./SettingsSidebar";
 
 const Settings = () => {
   const { currentTab, setCurrentTab, saveProfile, theme, toggleTheme } =
@@ -15,15 +15,14 @@ const Settings = () => {
     <SidebarProvider>
       <div className="flex flex-col h-screen w-full">
         <Header />
-        <div className="flex flex-col lg:flex-row h-screen">
-          <SettingsNavigationMenu
+        <div className="flex flex-col h-full">
+          <SettingsSidebar
             currentTab={currentTab}
             onTabChange={setCurrentTab}
-            className="lg:w-1/6 w-full h-1/4 lg:h-full"
+            className="flex-shrink-0 mx-4 mt-4"
           />
-
-          <div className="flex-1 flex flex-col h-auto lg:h-3/4">
-            <main className="flex-1 p-8">
+          <div className="flex-1 overflow-auto">
+            <main className="p-8">
               {currentTab === "profile" && <Profile onSave={saveProfile} />}
               {currentTab === "appearance" && (
                 <Appearance theme={theme} onThemeChange={toggleTheme} />
