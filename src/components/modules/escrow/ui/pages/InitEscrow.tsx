f42650dd22eef2544e0dc1b303fd476fd586af3d
Joel Vargas <vermudo.com@gmail.com>
2025-06-11 19:46:22 -0600

    feat: update InitializeEscrow component to conditionally render forms based on escrow type

--- src/components/modules/escrow/ui/pages/InitEscrow.tsx
+++ src/components/modules/escrow/ui/pages/InitEscrow.tsx
@@ -2,33 +2,32 @@
 
 import Loader from "@/components/utils/ui/Loader";
 import { useGlobalUIBoundedStore } from "@/core/store/ui";
-import InitializeEscrowForm from "../forms/InitializeEscrowForm";
+import { InitializeSingleEscrowForm } from "../forms/single-release/InitializeSingleEscrowForm";
+import { useEscrowUIBoundedStore } from "../../store/ui";
+import { InitializeMultiEscrowForm } from "../forms/multi-release/InitializeMultiEscrowForm";
 
 const InitializeEscrow = () => {
   const isLoading = useGlobalUIBoundedStore((state) => state.isLoading);
+  const escrowType = useEscrowUIBoundedStore((state) => state.escrowType);
+
+  if (isLoading) {
+    return <Loader isLoading={isLoading} />;
+  }
 
   return (
-    <>
-      {isLoading ? (
-        <Loader isLoading={isLoading} />
-      ) : (
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-3">
-            <div className="flex justify-between">
-              <h1 className="text-4xl font-bold">
-                Fill in the details of the Escrow
-              </h1>
-            </div>
-            <h2>
-              Fill in the details below to set up a secure and reliable escrow
-              agreement.
-            </h2>
-          </div>
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl font-bold">Fill in the details of the Escrow</h1>
+      <h2>
+        Fill in the details below to set up a secure and reliable escrow
+        agreement.
+      </h2>
 
-          <InitializeEscrowForm />
-        </div>
+      {escrowType === "single-release" ? (
+        <InitializeSingleEscrowForm />
+      ) : (
+        <InitializeMultiEscrowForm />
       )}
-    </>
+    </div>
   );
 };
 
