26fbdc8ddb4fad0923cbdc834681f59a8475e793
armandocodecr <totovip1464@gmail.com>
2025-06-13 14:45:20 -0600

    feat: enhance escrow service to fetch balances for multi-release and single-release types

--- src/components/modules/escrow/services/escrow.service.ts
+++ src/components/modules/escrow/services/escrow.service.ts
@@ -1,6 +1,7 @@
 import { BalanceItem, Escrow } from "@/@types/escrow.entity";
 import { getAllEscrowsByUser, updateEscrow } from "../server/escrow.firebase";
 import http from "@/core/config/axios/http";
+import { MultiReleaseEscrow, SingleReleaseEscrow } from "@trustless-work/escrow";
 
 export const fetchAllEscrows = async ({
   address,
@@ -18,25 +19,43 @@ export const fetchAllEscrows = async ({
     typeof isActive === "boolean"
       ? escrowsByUser.data.filter((e: Escrow) => e.isActive === isActive)
       : escrowsByUser.data;
+  console.log({ filtered })
+  const multiReleaseEscrow = filtered.filter((escrow: Escrow) => escrow.type === "multi-release");
+  const singleReleaseEscrow = filtered.filter((escrow: Escrow) => escrow.type === "single-release");
 
-  const contractIds = filtered.map((escrow: Escrow) => escrow.contractId);
+  const multiReleaseEscrowContractsId = multiReleaseEscrow.map((escrow: Escrow) => escrow.contractId);
+  const singleReleaseEscrowContractsId = singleReleaseEscrow.map((escrow: Escrow) => escrow.contractId);
 
-  if (!Array.isArray(contractIds)) {
+  if (!Array.isArray(multiReleaseEscrowContractsId)) {
     throw new Error("contractIds is not a valid array.");
   }
 
-  const { data } = await http.get(
+  if (!Array.isArray(singleReleaseEscrowContractsId)) {
+    throw new Error("contractIds is not a valid array.");
+  }
+
+  const { data: singleData } = await http.get(
     "/escrow/single-release/get-multiple-escrow-balance",
     {
-      params: { addresses: contractIds, signer: address || "" },
+      params: { addresses: singleReleaseEscrowContractsId, signer: address || "" },
+    },
+  );
+  
+  const { data: multiData } = await http.get(
+    "/escrow/multi-release/get-multiple-escrow-balance",
+    {
+      params: { addresses: multiReleaseEscrowContractsId, signer: address || "" },
     },
   );
 
-  const balances = data as unknown as BalanceItem[];
+  const singleBalances = singleData as unknown as BalanceItem[];
+  const multiBalances = multiData as unknown as BalanceItem[];
+
+  const balancesMerged = [...singleBalances, ...multiBalances];
 
   return Promise.all(
     filtered.map(async (escrow: Escrow) => {
-      const matchedBalance = balances.find(
+      const matchedBalance = balancesMerged.find(
         (item) => item.address === escrow.contractId,
       );
       const plainBalance = matchedBalance ? matchedBalance.balance : 0;
