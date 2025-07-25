5f0feeb154e24ffc6f79307a1d243a4f66495a5d
JoelVR17 <vermudo.com@gmail.com>
2025-07-25 13:15:52 -0600

    remove: delete ReportIssuePage component as it is no longer needed

--- src/app/dashboard/report-issue/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import ReportIssueLogged from "@/components/modules/report-issue/ui/pages/ReportIssueLogged";
-
-const ReportIssuePage: React.FC = () => {
-  return <ReportIssueLogged />;
-};
-
-export default ReportIssuePage;
