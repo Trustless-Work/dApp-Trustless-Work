bea9ff55d2083af9e402406c36431e50ba1ac525
Joel Vargas <vermudo.com@gmail.com>
2025-08-01 12:04:13 -0600

    feat(wallet): add AlbedoModule to wallet kit for expanded wallet support

--- src/components/modules/auth/wallet/constants/wallet-kit.constant.ts
+++ src/components/modules/auth/wallet/constants/wallet-kit.constant.ts
@@ -3,10 +3,11 @@ import {
   WalletNetwork,
   FREIGHTER_ID,
   FreighterModule,
+  AlbedoModule,
 } from "@creit.tech/stellar-wallets-kit";
 
 export const kit: StellarWalletsKit = new StellarWalletsKit({
   network: WalletNetwork.TESTNET,
   selectedWalletId: FREIGHTER_ID,
-  modules: [new FreighterModule()],
+  modules: [new FreighterModule(), new AlbedoModule()],
 });
