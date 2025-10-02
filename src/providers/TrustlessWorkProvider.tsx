7c50568574b091c172569358d45696afcc7d34a9
Joel Vargas <vermudo.com@gmail.com>
2025-10-02 13:04:22 -0600

    fix: testnet reqts

--- src/providers/TrustlessWorkProvider.tsx
+++ src/providers/TrustlessWorkProvider.tsx
@@ -14,17 +14,22 @@ interface TrustlessWorkProviderProps {
 export function TrustlessWorkProvider({
   children,
 }: TrustlessWorkProviderProps) {
-  const apiKey = process.env.NEXT_PUBLIC_ESCROW_MANAGER_API_KEY || "";
-
   const currentNetwork =
     typeof window !== "undefined"
       ? (localStorage.getItem("network") as "testnet" | "mainnet") || "testnet"
       : "testnet";
 
+  const apiKeyTestnet =
+    process.env.NEXT_PUBLIC_ESCROW_MANAGER_API_KEY_TESTNET || "";
+  const apiKeyMainnet =
+    process.env.NEXT_PUBLIC_ESCROW_MANAGER_API_KEY_MAINNET || "";
   const baseURL = currentNetwork === "mainnet" ? mainNet : development;
 
   return (
-    <TrustlessWorkConfig baseURL={baseURL} apiKey={apiKey}>
+    <TrustlessWorkConfig
+      baseURL={baseURL}
+      apiKey={currentNetwork === "mainnet" ? apiKeyMainnet : apiKeyTestnet}
+    >
       {children}
     </TrustlessWorkConfig>
   );
