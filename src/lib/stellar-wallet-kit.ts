b51124b36f0c37c788448b9ab20fe70141a3ef00
Joel Vargas <vermudo.com@gmail.com>
2025-10-02 14:03:02 -0600

    feat: add more wallets

--- src/lib/stellar-wallet-kit.ts
+++ src/lib/stellar-wallet-kit.ts
@@ -3,13 +3,35 @@ import {
   FREIGHTER_ID,
   FreighterModule,
   AlbedoModule,
+  xBullModule,
+  LobstrModule,
   WalletNetwork,
 } from "@creit.tech/stellar-wallets-kit";
+import { LedgerModule } from "@creit.tech/stellar-wallets-kit/modules/ledger.module";
+import {
+  WalletConnectAllowedMethods,
+  WalletConnectModule,
+} from "@creit.tech/stellar-wallets-kit/modules/walletconnect.module";
 
 export const kit: StellarWalletsKit = new StellarWalletsKit({
   network: WalletNetwork.TESTNET,
   selectedWalletId: FREIGHTER_ID,
-  modules: [new FreighterModule(), new AlbedoModule()],
+  modules: [
+    new FreighterModule(),
+    new AlbedoModule(),
+    new WalletConnectModule({
+      url: "https://dapp.trustlesswork.com",
+      projectId: "fa57d523d12455e4fc2c8c83c94ec7b1",
+      method: WalletConnectAllowedMethods.SIGN,
+      description: "Trustless Work",
+      name: "Trustless Work",
+      icons: ["/favicon.ico"],
+      network: WalletNetwork.TESTNET,
+    }),
+    new xBullModule(),
+    new LedgerModule(),
+    new LobstrModule(),
+  ],
 });
 
 interface signTransactionProps {
